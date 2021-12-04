457b0601e44b9615f773340a1f088fefcec10b9d
Shaleen Jain <shaleen@jain.sh>
2021-12-04 08:24:12 +0000

    feat: add arbitrum L2 bridge

---
+++ contracts/src/bridge/arbitrum/L2Bridge.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.0;
+
+import "./interfaces/IArbSys.sol";
+
+contract L2Bridge {
+    address public l1Target;
+    IArbSys constant arbsys = IArbSys(address(100));
+
+    event L2ToL1TxCreated(uint256 indexed withdrawalId);
+
+    constructor(address _l1Target) public {
+        l1Target = _l1Target;
+    }
+
+    /**
+     * Sends an arbitrary message from one domain to another.
+     *
+     * @param _calldata The L1 encoded message data.
+     * @return Unique id to track the message request/transaction.
+     */
+    function sendCrossDomainMessage(bytes memory _calldata) external payable returns (uint256) {
+        uint256 withdrawalId = arbsys.sendTxToL1(l1Target, _calldata);
+
+        emit L2ToL1TxCreated(withdrawalId);
+        return withdrawalId;
+    }
+}
