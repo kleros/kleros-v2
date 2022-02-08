7020cd13d26f5ef3a42c0ce1e965d203efa9ef4d
jaybuidl <jb@kleros.io>
2022-02-08 00:12:15 +0000

    refactor: minor renames
    contract SafeArbitrumBridge --> SafeBridgeArbitrum
    contract SafeGnosisBridge --> SafeBridgeGnosis
    function sendCrossDomainMessage() --> sendSafe()
    event DisputeHash --> OutgoingDispute
    event OutboxMessage --> OutgoingMessage
    event ClaimMade --> ClaimReceived
    parameter messagePreImage --> message

--- contracts/src/bridge/SafeBridgeGnosis.sol
+++ contracts/src/bridge/SafeBridgeGnosis.sol
@@ -13,19 +13,14 @@ pragma solidity ^0.8.0;
 import "./interfaces/gnosis-chain/IAMB.sol";
 import "./interfaces/ISafeBridge.sol";
 
-contract SafeGnosisBridge is ISafeBridge {
+contract SafeBridgeGnosis is ISafeBridge {
     IAMB amb;
 
     constructor(IAMB _amb) {
         amb = _amb;
     }
 
-    function sendCrossDomainMessage(address _receiver, bytes memory _calldata)
-        external
-        payable
-        override
-        returns (uint256)
-    {
+    function sendSafe(address _receiver, bytes memory _calldata) external payable override returns (uint256) {
         bytes32 id = amb.requireToPassMessage(_receiver, _calldata, amb.maxGasPerTx());
         return uint256(id);
     }
