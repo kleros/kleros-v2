d553130ca1c0f33a0bcdc8e6c23e3e160ce79985
jaybuidl <jb@kleros.io>
2022-02-08 17:46:37 +0000

    fix: eslint and prettify

--- contracts/deploy/01-foreign-chain.ts
+++ contracts/deploy/01-foreign-chain.ts
@@ -1,31 +1,31 @@
-import {parseEther} from 'ethers/lib/utils';
+import { parseEther } from "ethers/lib/utils";
 
-import {HardhatRuntimeEnvironment} from 'hardhat/types';
-import {DeployFunction} from 'hardhat-deploy/types';
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
 
 import getContractAddress from "../deploy-helpers/getContractAddress";
 
 const FOREIGN_CHAIN_IDS = [1, 4];
 const paramsByChainId = {
   1: {
-    claimDeposit: parseEther('0.1'),
+    claimDeposit: parseEther("0.1"),
     challengeDuration: 86400, // 1 day
     homeChainId: 42161,
   },
   4: {
-    claimDeposit: parseEther('0.1'),
+    claimDeposit: parseEther("0.1"),
     challengeDuration: 3600, // 1 hour
     homeChainId: 421611,
   },
 };
 
-const deployForeignGateway: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const {ethers, deployments, getNamedAccounts, getChainId, config} = hre;
-  const {deploy} = deployments;
+const deployForeignGateway: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
+  const { ethers, deployments, getNamedAccounts, getChainId, config } = hre;
+  const { deploy } = deployments;
   const { providers, constants } = ethers;
   const { hexZeroPad } = hre.ethers.utils;
 
-  const {deployer } = await getNamedAccounts();
+  const { deployer } = await getNamedAccounts();
   const chainId = await getChainId();
 
   const homeNetworks = {
@@ -36,7 +36,7 @@ const deployForeignGateway: DeployFunction = async function (hre: HardhatRuntime
   const homeChainProvider = new providers.JsonRpcProvider(url);
   const nonce = await homeChainProvider.getTransactionCount(deployer);
 
-  const { claimDeposit, challengeDuration, homeChainId} = paramsByChainId[chainId];
+  const { claimDeposit, challengeDuration, homeChainId } = paramsByChainId[chainId];
 
   // home Gateway deploy tx will the third tx after this on it's network,
   // so we add two to the current nonce.
@@ -46,13 +46,13 @@ const deployForeignGateway: DeployFunction = async function (hre: HardhatRuntime
   console.log(nonce + 2);
   console.log(homeGatewayAddress);
 
-  let fastBridgeReceiver = await deploy('FastBridgeReceiver', {
+  const fastBridgeReceiver = await deploy("FastBridgeReceiver", {
     from: deployer,
     args: [deployer, claimDeposit, challengeDuration],
     log: true,
   });
 
-  let foreignGateway = await deploy('ForeignGateway', {
+  const foreignGateway = await deploy("ForeignGateway", {
     from: deployer,
     args: [deployer, fastBridgeReceiver.address, ["1000", "10000"], homeGatewayAddress, homeChainIdAsBytes32],
     log: true,
