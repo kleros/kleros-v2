c40426fa333beb751906f8bb3ad754efa847220b
shotaro <shawtarohgn@gmail.com>
2022-03-11 10:36:10 +0000

    Update FastBridgeSender.sol
    reverting my commits to maintain the bridge reciever as relatively app agnostic, but still need to implement a contorl flow to relay correct dispute rulings directly from kleros core

--- contracts/src/bridge/FastBridgeSender.sol
+++ contracts/src/bridge/FastBridgeSender.sol
@@ -13,38 +13,27 @@ pragma solidity ^0.8.0;
 import "./interfaces/ISafeBridge.sol";
 import "./interfaces/IFastBridgeSender.sol";
 import "./interfaces/IFastBridgeReceiver.sol";
-import "../gateway/interfaces/IForeignGateway.sol"
-import "../gateway/HomeGateway.sol"
-import "../arbitration/KlerosCore.sol";
 
 contract FastBridgeSender is IFastBridgeSender {
     ISafeBridge public safebridge;
     IFastBridgeReceiver public fastBridgeReceiver;
     address public fastSender;
-    address public foreignGateway;
-    HomeGateway public homeGateway;
-    KlerosCore public core; // The Kleros Core arbitrator
-    
+
     /**
      * The bridgers need to watch for these events and
      * relay the messageHash on the FastBridgeReceiver.
      */
     event OutgoingMessage(address target, bytes32 messageHash, bytes message);
 
-    constructor(ISafeBridge _safebridge, IFastBridgeReceiver _fastBridgeReceiver, KlerosCore _core, address, _foreignGateway, HomeGateway _homeGateway) {
+    constructor(ISafeBridge _safebridge, IFastBridgeReceiver _fastBridgeReceiver) {
         safebridge = _safebridge;
         fastBridgeReceiver = _fastBridgeReceiver;
-        core = _core;
-        foreignGateway = _foreignGateway;
-        homeGateway = _homeGateway;
     }
 
     function setFastSender(address _fastSender) external {
         require(fastSender == address(0));
         fastSender = _fastSender;
     }
-    
-
 
     /**
      * Sends an arbitrary message from one domain to another
@@ -53,13 +42,13 @@ contract FastBridgeSender is IFastBridgeSender {
      * @param _receiver The L1 contract address who will receive the calldata
      * @param _calldata The receiving domain encoded message data.
      */
-    function sendFast(bytes memory _calldata) external {
+    function sendFast(address _receiver, bytes memory _calldata) external {
         require(msg.sender == fastSender, "Access not allowed: Fast Sender only.");
 
         // Encode the receiver address with the function signature + arguments i.e calldata
-        bytes memory encodedData = abi.encode(foreignGateway, _calldata);
+        bytes memory encodedData = abi.encode(_receiver, _calldata);
 
-        emit OutgoingMessage(foreignGateway, keccak256(encodedData), encodedData);
+        emit OutgoingMessage(_receiver, keccak256(encodedData), encodedData);
     }
 
     /**
@@ -74,7 +63,7 @@ contract FastBridgeSender is IFastBridgeSender {
      * @param _receiver The L1 contract address who will receive the calldata
      * @param _calldata The receiving domain encoded message data.
      */
-    function sendSafe(uint256 _disputeID) external payable {
+    function sendSafe(address _receiver, bytes memory _calldata) external payable {
         // The safe bridge sends the encoded data to the FastBridgeReceiver
         // in order for the FastBridgeReceiver to resolve any potential
         // challenges and then forwards the message to the actual
@@ -84,21 +73,8 @@ contract FastBridgeSender is IFastBridgeSender {
         // TODO: add access checks for this on the FastBridgeReceiver.
         // TODO: how much ETH should be provided for bridging? add an ISafeBridge.bridgingCost()
 
-
-        Dispute storage dispute = core.disputes[_disputeID];
-        //require(dispute.ruled, "Ruling not requested for execution.");
-        
-
-        bytes32 disputeHash = homeGateway.disputeIDtoHash[_disputeID];
-        RelayedData memory relayedData = homeGateway.disputeHashtoRelayedData[disputeHash];
-        uint256 ruling = core.currentRuling(_disputeID);
-        
-        bytes4 methodSelector = IForeignGateway.relayRule.selector;
-        bytes memory data = abi.encodeWithSelector(methodSelector, disputeHash, ruling, relayedData.relayer);
-        
-        bytes memory encodedData = abi.encode(foreignGateway, data);
-
-        bytes memory encodedTxData = abi.encodeWithSelector(fastBridgeReceiver.relayRule.selector, dispute.ruled, encodedData);
+        bytes memory encodedData = abi.encode(_receiver, _calldata);
+        bytes memory encodedTxData = abi.encodeWithSelector(fastBridgeReceiver.relayRule.selector, encodedData);
         safebridge.sendSafe{value: msg.value}(address(fastBridgeReceiver), encodedTxData);
     }
 }
