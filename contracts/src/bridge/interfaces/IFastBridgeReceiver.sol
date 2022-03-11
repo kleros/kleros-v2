81dafdbbb01fd27591422a65d9f5f691dff9794a
shotaro <shawtarohgn@gmail.com>
2022-03-11 18:49:54 +0000

    Update IFastBridgeReceiver.sol

--- contracts/src/bridge/interfaces/IFastBridgeReceiver.sol
+++ contracts/src/bridge/interfaces/IFastBridgeReceiver.sol
@@ -3,13 +3,13 @@
 pragma solidity ^0.8.0;
 
 interface IFastBridgeReceiver {
-    function claim(bytes32 _messageHash) external payable;
+    function claim(uint256 _fastMessageIndex, bytes32 _messageHash) external payable;
 
-    function verifyAndRelay(bytes32 _messageHash, bytes memory _calldata) external;
+    function verifyAndRelay(uint _fastMessageIndex, bytes memory _encodedData) external;
 
-    function relayRule(bytes memory _calldata) external;
+    function relayRule(bytes memory _encodedData) external;
 
-    function withdrawClaimDeposit(bytes32 _messageHash) external;
+    function withdrawClaimDeposit(uint256 _fastMessageIndex) external;
 
     function viewClaimDeposit() external view returns (uint256 amount);
 }
