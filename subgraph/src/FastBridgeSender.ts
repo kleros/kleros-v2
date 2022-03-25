2ef1aef2f57b2095e4782bf85fc25c5e0821e594
alcercu <alcercu@alumni.upv.es>
2022-03-25 18:25:56 +0100

    feat(subgraph): add HomeGateway and FastBridgeSender mappings

---
+++ subgraph/src/FastBridgeSender.ts
@@ -0,0 +1,16 @@
+import {
+  OutgoingMessage as OutgoingMessageEvent,
+} from "../generated/FastBridgeSender/FastBridgeSender";
+import {
+  OutgoingMessage
+} from "../generated/schema";
+
+export function handleOutgoingMessage(event: OutgoingMessageEvent): void {
+  const outgoingMessage = new OutgoingMessage(
+    event.params.messageHash.toString()
+  );
+  outgoingMessage.target = event.params.target;
+  outgoingMessage.messageHash = event.params.messageHash.toHex();
+  outgoingMessage.message = event.params.message.toHex();
+  outgoingMessage.save();
+}
