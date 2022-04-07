8b1856ccbd14f7b3639a4853e19b52885426cf95
alcercu <alcercu@alumni.upv.es>
2022-04-07 17:45:44 +0200

    feat(web): add routing

---
+++ web/src/pages/home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100vw;
+  height: 300px;
+  background-color: ${({ theme }) => theme.lightBackground};
+`;
+
+const Home: React.FC = () => (
+  <Container></Container>
+);
+
+export default Home;
