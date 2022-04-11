a71f199c476d4281c6bb0d0fc05000ca3880122a
alcercu <alcercu@alumni.upv.es>
2022-04-11 19:51:59 +0200

    feat(web/components): add LightButton

---
+++ web/src/components/LightButton.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import styled from "styled-components";
+import { Button } from "@kleros/ui-components-library";
+
+const StyledButton = styled(Button)`
+  background-color: transparent;
+  padding-left: 0;
+  .button-text {
+    color: ${({ theme }) => theme.primaryText};
+    font-weight: 400;
+  }
+  .button-svg {
+    fill: ${({ theme }) => theme.secondaryPurple};
+  }
+
+  :focus,
+  :hover {
+    background-color: transparent;
+  }
+`;
+
+interface ILightButton {
+  text: string;
+  icon?: (className: string) => React.ReactNode;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  disabled?: boolean;
+}
+
+const LightButton: React.FC<ILightButton> = ({
+  text,
+  icon,
+  onClick,
+  disabled,
+}) => (
+  <StyledButton
+    variant="primary"
+    small
+    {...{ text, icon, onClick, disabled }}
+  />
+);
+
+export default LightButton;
