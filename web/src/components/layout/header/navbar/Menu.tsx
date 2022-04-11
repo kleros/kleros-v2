526ea7859a2deb571cbc0dddf4e336377319c4ce
alcercu <alcercu@alumni.upv.es>
2022-04-11 19:53:59 +0200

    feat(web): add navbar menu icons and implement theme switch

--- web/src/components/layout/header/navbar/Menu.tsx
+++ web/src/components/layout/header/navbar/Menu.tsx
@@ -1,5 +1,12 @@
 import React from "react";
 import styled from "styled-components";
+import { useToggleTheme } from "hooks/useToggleThemeContext";
+import LightButton from "components/LightButton";
+import NotificationsIcon from "svgs/menu-icons/notifications.svg";
+import DarkModeIcon from "svgs/menu-icons/dark-mode.svg";
+import LightModeIcon from "svgs/menu-icons/light-mode.svg";
+import HelpIcon from "svgs/menu-icons/help.svg";
+import SettingsIcon from "svgs/menu-icons/settings.svg";
 
 const Container = styled.div``;
 
@@ -9,31 +16,32 @@ const ButtonContainer = styled.div`
   align-items: center;
 `;
 
-const StyledButton = styled.button`
-  border: none;
-  background-color: transparent;
-  p {
-    margin: 0;
-  }
-`;
+const Explore: React.FC = () => {
+  const [theme, toggleTheme] = useToggleTheme();
+  const isLightTheme = theme === "light";
+  const buttons = [
+    { text: "Notifications", icon: NotificationsIcon },
+    { text: "Settings", icon: SettingsIcon },
+    { text: "Help", icon: HelpIcon },
+    {
+      text: `${isLightTheme ? "Dark" : "Light"} Mode`,
+      icon: isLightTheme ? DarkModeIcon : LightModeIcon,
+      onClick: () => toggleTheme(),
+    },
+  ];
 
-const buttons = [
-  { text: "Notifications", icon: "Cases" },
-  { text: "Settings", icon: "Courts" },
-  { text: "Help", icon: "Dashboard" },
-  { text: "Dark Mode", icon: "Dashboard" },
-];
-
-const Explore: React.FC = () => (
-  <Container>
-    {buttons.map(({ text }) => (
-      <ButtonContainer key={text}>
-        <StyledButton>
-          <p>{text}</p>
-        </StyledButton>
-      </ButtonContainer>
-    ))}
-  </Container>
-);
+  return (
+    <Container>
+      {buttons.map(({ text, icon: Icon, onClick }) => (
+        <ButtonContainer key={text}>
+          <LightButton
+            {...{ text, onClick }}
+            icon={(className: string) => <Icon {...{ className }} />}
+          />
+        </ButtonContainer>
+      ))}
+    </Container>
+  );
+};
 
 export default Explore;
