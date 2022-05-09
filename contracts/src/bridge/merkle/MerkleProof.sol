56d5a38bc2617fed28cc4b0dec140756bf09a461
shotaronowhere <shawtarohgn@gmail.com>
2022-05-09 19:18:53 +0100

    feat: hash pair ordering

--- contracts/src/bridge/merkle/MerkleProof.sol
+++ contracts/src/bridge/merkle/MerkleProof.sol
@@ -16,67 +16,55 @@ pragma solidity ^0.8.0;
  *  @dev A set of funcitons to verify merkle proofs.
  */
 contract MerkleProof {
-
     /** @dev Validates membership of leaf in merkle tree with merkle proof.
      *  @param proof The merkle proof.
-     *  @param route The index of item in the merkle tree.
      *  @param leaf The leaf to validate membership in merkle tree.
      *  @param merkleRoot The root of the merkle tree.
      */
     function validateProof(
         bytes32[] memory proof,
-        uint256 route,
         bytes32 leaf,
         bytes32 merkleRoot
-    ) internal pure returns (bool){
-        return(merkleRoot == calculateRoot(proof,route,leaf));
+    ) internal pure returns (bool) {
+        return (merkleRoot == calculateRoot(proof, leaf));
     }
 
     /** @dev Validates membership of leaf in merkle tree with merkle proof.
      *  @param proof The merkle proof.
-     *  @param route The index of item in the merkle tree.
      *  @param data The data to validate membership in merkle tree.
      *  @param merkleRoot The root of the merkle tree.
      */
     function validateProof(
         bytes32[] memory proof,
-        uint256 route,
         bytes memory data,
         bytes32 merkleRoot
-    ) public pure returns (bool){
-        return validateProof(proof, route, sha256(data), merkleRoot);
+    ) public pure returns (bool) {
+        return validateProof(proof, sha256(data), merkleRoot);
     }
 
     /** @dev Calculates merkle root from proof.
      *  @param proof The merkle proof.
-     *  @param route The index of item in the merkle tree.
      *  @param leaf The leaf to validate membership in merkle tree..
      */
-    function calculateRoot(
-        bytes32[] memory proof,
-        uint256 route,
-        bytes32 leaf
-    ) internal pure returns (bytes32) {
+    function calculateRoot(bytes32[] memory proof, bytes32 leaf) internal pure returns (bytes32) {
         uint256 proofLength = proof.length;
         require(proofLength <= 32, "Invalid Proof");
         bytes32 h = leaf;
         for (uint256 i = 0; i < proofLength; i++) {
-            bytes32 evidence = proof[i];
+            bytes32 proofElement = proof[i];
             // effecient hash
-            if (route % 2 == 0) {
+            if (proofElement > h)
                 assembly {
                     mstore(0x00, h)
-                    mstore(0x20, evidence)
+                    mstore(0x20, proofElement)
                     h := keccak256(0x00, 0x40)
                 }
-            } else {
+            else
                 assembly {
-                    mstore(0x00, evidence)
+                    mstore(0x00, proofElement)
                     mstore(0x20, h)
                     h := keccak256(0x00, 0x40)
                 }
-            }
-            route /= 2;
         }
         return h;
     }
