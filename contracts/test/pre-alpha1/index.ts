74b99eecf5754cd27b490963d458becdae888b81
jaybuidl <jb@kleros.io>
2022-05-17 17:11:36 +0100

    test: fixed broken test using a hardcoded hash based on an unpredictable block number

--- contracts/test/pre-alpha1/index.ts
+++ contracts/test/pre-alpha1/index.ts
@@ -1,6 +1,6 @@
 import { expect } from "chai";
 import { deployments, ethers, getNamedAccounts, network } from "hardhat";
-import { BigNumber } from "ethers";
+import { BigNumber, utils } from "ethers";
 import {
   IncrementalNG,
   PNK,
@@ -11,7 +11,7 @@ import {
   FastBridgeSenderToEthereum,
   HomeGatewayToEthereum,
   ArbSysMock,
-  InboxMock
+  InboxMock,
 } from "../../typechain-types";
 import { OutgoingMessage } from "http";
 
@@ -130,7 +130,6 @@ describe("Demo pre-alpha1", function () {
     expect(tx2).to.emit(homeGateway, "Dispute");
     const events2 = (await tx2.wait()).events;
 
-
     const tx3 = await core.draw(0, 1000);
     const events3 = (await tx3.wait()).events;
 
@@ -148,16 +147,16 @@ describe("Demo pre-alpha1", function () {
     await core.passPeriod(0);
     expect((await core.disputes(0)).period).to.equal(Period.execution);
     await core.execute(0, 0, 1000);
-    let ticket1 = await fastBridgeSender.currentTicketID();
+    const ticket1 = await fastBridgeSender.currentTicketID();
     expect(ticket1).to.equal(1);
 
     const tx4 = await core.executeRuling(0);
     expect(tx4).to.emit(fastBridgeSender, "OutgoingMessage");
 
-    let event5 = await fastBridgeSender.queryFilter(OutgoingMessage);
+    const event5 = await fastBridgeSender.queryFilter(OutgoingMessage);
     console.log("Executed ruling");
 
-    let ticket2 = await fastBridgeSender.currentTicketID();
+    const ticket2 = await fastBridgeSender.currentTicketID();
     expect(ticket2).to.equal(2);
 
     const ticketID = event5[0].args.ticketID;
@@ -165,26 +164,23 @@ describe("Demo pre-alpha1", function () {
     const blockNumber = event5[0].args.blockNumber;
     const messageData = event5[0].args.message;
 
-    let bridgerBalance = await ethers.provider.getBalance(bridger.address);
-    //bridger tx starts - Honest Bridger 
+    const bridgerBalance = await ethers.provider.getBalance(bridger.address);
+    // bridger tx starts - Honest Bridger 
     const tx5 = await fastBridgeReceiver.connect(bridger).claim(ticketID, messageHash, { value: ONE_TENTH_ETH });
-    let blockNumBefore = await ethers.provider.getBlockNumber();
-    let blockBefore = await ethers.provider.getBlock(blockNumBefore);
-    let timestampBefore = blockBefore.timestamp;
+    const blockNumBefore = await ethers.provider.getBlockNumber();
+    const blockBefore = await ethers.provider.getBlock(blockNumBefore);
+    const timestampBefore = blockBefore.timestamp;
     expect(tx5).to.emit(fastBridgeReceiver, "ClaimReceived").withArgs(ticketID, messageHash, timestampBefore);
 
-
-    //wait for challenge period to pass
+    // wait for challenge period to pass
     await network.provider.send("evm_increaseTime", [300]);
     await network.provider.send("evm_mine");
-
 
     const tx7 = await fastBridgeReceiver.connect(bridger).verifyAndRelay(ticketID, blockNumber, messageData);
     expect(tx7).to.emit(arbitrable, "Ruling");
 
     const tx8 = await fastBridgeReceiver.withdrawClaimDeposit(ticketID);
   });
-
 
   it("Demo - Honest Claim - Challenged - Bridger Paid, Challenger deposit forfeited", async () => {
     const arbitrationCost = ONE_TENTH_ETH.mul(3);
@@ -227,7 +223,7 @@ describe("Demo pre-alpha1", function () {
     expect(tx).to.emit(foreignGateway, "OutgoingDispute"); //.withArgs(disputeId, deployer.address);
     console.log(`disputeId: ${disputeId}`);
 
-    let events = await foreignGateway.queryFilter(OutgoingMessage);
+    const events = await foreignGateway.queryFilter(OutgoingMessage);
 
 
     const lastBlock = await ethers.provider.getBlock(tx.blockNumber - 1);
@@ -261,9 +257,8 @@ describe("Demo pre-alpha1", function () {
     await core.passPeriod(1);
     expect((await core.disputes(1)).period).to.equal(Period.execution);
     await core.execute(1, 0, 1000);
-    let ticket1 = await fastBridgeSender.currentTicketID();
+    const ticket1 = await fastBridgeSender.currentTicketID();
     expect(ticket1).to.equal(2);
-
 
     const tx4 = await core.executeRuling(1);
 
@@ -271,18 +266,26 @@ describe("Demo pre-alpha1", function () {
 
     console.log("Executed ruling");
 
-    let ticket2 = await fastBridgeSender.currentTicketID();
+    const ticket2 = await fastBridgeSender.currentTicketID();
     expect(ticket2).to.equal(3);
-    let eventFilter = fastBridgeSender.filters.OutgoingMessage();
-    let event5 = await fastBridgeSender.queryFilter(eventFilter, "latest");
-
-    let event6 = await ethers.provider.getLogs(eventFilter);
+    const eventFilter = fastBridgeSender.filters.OutgoingMessage();
+    const event5 = await fastBridgeSender.queryFilter(eventFilter, "latest");
+    const event6 = await ethers.provider.getLogs(eventFilter);
 
     const ticketID = event5[0].args.ticketID.toNumber();
     const messageHash = event5[0].args.messageHash;
     const blockNumber = event5[0].args.blockNumber;
     const messageData = event5[0].args.message;
-    let currentID = await fastBridgeSender.currentTicketID();
+    console.log("TicketID: %d", ticketID);
+    console.log("Block: %d", blockNumber);
+    console.log("Message Data: %s", messageData);
+    console.log("Message Hash: %s", messageHash);
+    const expectedHash = utils.keccak256(
+      utils.defaultAbiCoder.encode(["uint256", "uint256", "bytes"], [ticketID, blockNumber, messageData])
+    );
+    expect(messageHash).to.equal(expectedHash);
+
+    const currentID = await fastBridgeSender.currentTicketID();
     expect(currentID).to.equal(3);
 
     // bridger tx starts
@@ -297,25 +300,28 @@ describe("Demo pre-alpha1", function () {
     blockNumBefore = await ethers.provider.getBlockNumber();
     blockBefore = await ethers.provider.getBlock(blockNumBefore);
     timestampBefore = blockBefore.timestamp;
-
+    console.log("Block: %d", blockNumBefore);
     expect(tx6).to.emit(fastBridgeReceiver, "ClaimChallenged").withArgs(ticketID, timestampBefore);
 
-    //wait for challenge period to pass
+    // wait for challenge period to pass
     await network.provider.send("evm_increaseTime", [300]);
     await network.provider.send("evm_mine");
 
-
-    await expect(fastBridgeReceiver.connect(bridger).verifyAndRelay(ticketID, blockNumber, messageData)).to.be.revertedWith('Claim is challenged');
+    await expect(
+      fastBridgeReceiver.connect(bridger).verifyAndRelay(ticketID, blockNumber, messageData)
+    ).to.be.revertedWith("Claim is challenged");
     await fastBridgeSender.set_arb(arbsys.address);
 
-    let data = await ethers.utils.defaultAbiCoder.decode(["address", "bytes"], messageData);
-    let tx7 = await fastBridgeSender.connect(bridger).sendSafeFallbackMock(ticketID, foreignGateway.address, data[1], { gasLimit: 1000000 }
-    );
+    const data = await ethers.utils.defaultAbiCoder.decode(["address", "bytes"], messageData);
+    const tx7 = await fastBridgeSender
+      .connect(bridger)
+      .sendSafeFallbackMock(ticketID, foreignGateway.address, data[1], { gasLimit: 1000000 });
     expect(tx7).to.emit(fastBridgeSender, "L2ToL1TxCreated");
     expect(tx7).to.emit(arbitrable, "Ruling");
 
-    await expect(fastBridgeReceiver.withdrawChallengeDeposit(ticketID)).to.be.revertedWith('Claim verified: deposit forfeited');
-
+    await expect(fastBridgeReceiver.withdrawChallengeDeposit(ticketID)).to.be.revertedWith(
+      "Claim verified: deposit forfeited"
+    );
   });
 
   it("Demo - Dishonest Claim - Challenged - Bridger deposit forfeited, Challenger paid", async () => {
@@ -358,9 +364,8 @@ describe("Demo pre-alpha1", function () {
     expect(tx).to.emit(foreignGateway, "DisputeCreation"); //.withArgs(disputeId, deployer.address);
     expect(tx).to.emit(foreignGateway, "OutgoingDispute"); //.withArgs(disputeId, deployer.address);
     console.log(`disputeId: ${disputeId}`);
-    let coreId = disputeId - 1;
+    const coreId = disputeId - 1;
     // let events = await foreignGateway.queryFilter(OutgoingMessage);
-
 
     const lastBlock = await ethers.provider.getBlock(tx.blockNumber - 1);
     const disputeHash = ethers.utils.solidityKeccak256(
@@ -393,9 +398,8 @@ describe("Demo pre-alpha1", function () {
     await core.passPeriod(coreId);
     expect((await core.disputes(coreId)).period).to.equal(Period.execution);
     await core.execute(coreId, 0, 1000);
-    let ticket1 = await fastBridgeSender.currentTicketID();
+    const ticket1 = await fastBridgeSender.currentTicketID();
     expect(ticket1).to.equal(3);
-
 
     const tx4 = await core.executeRuling(coreId);
 
@@ -403,29 +407,39 @@ describe("Demo pre-alpha1", function () {
 
     console.log("Executed ruling");
 
-    let ticket2 = await fastBridgeSender.currentTicketID();
+    const ticket2 = await fastBridgeSender.currentTicketID();
     expect(ticket2).to.equal(4);
-    let eventFilter = fastBridgeSender.filters.OutgoingMessage();
-    let event5 = await fastBridgeSender.queryFilter(eventFilter, "latest");
-
-    let event6 = await ethers.provider.getLogs(eventFilter);
+    const eventFilter = fastBridgeSender.filters.OutgoingMessage();
+    const event5 = await fastBridgeSender.queryFilter(eventFilter, "latest");
+    const event6 = await ethers.provider.getLogs(eventFilter);
 
     const ticketID = event5[0].args.ticketID.toNumber();
     const messageHash = event5[0].args.messageHash;
     const blockNumber = event5[0].args.blockNumber;
     const messageData = event5[0].args.message;
-    let currentID = await fastBridgeSender.currentTicketID();
+    console.log("TicketID: %d", ticketID);
+    console.log("Block: %d", blockNumber);
+    console.log("Message Data: %s", messageData);
+    console.log("Message Hash: %s", messageHash);
+    const expectedHash = utils.keccak256(
+      utils.defaultAbiCoder.encode(["uint256", "uint256", "bytes"], [ticketID, blockNumber, messageData])
+    );
+    expect(messageHash).to.equal(expectedHash);
+
+    const currentID = await fastBridgeSender.currentTicketID();
     expect(currentID).to.equal(4);
 
-    //bridger tx starts - bridger creates fakeData & fakeHash for dishonest ruling
+    // bridger tx starts - bridger creates fakeData & fakeHash for dishonest ruling
     const fakeData = "0x0000000000000000000000009a9f2ccfde556a7e9ff0848998aa4a0cfd8863ae000000000000000000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000000643496987923bd6a8aa2bdce6c5b15551665079e7acfb1b4d2149ac7e2f72260417d541b7f000000000000000000000000000000000000000000000000000000000000000100000000000000000000000070997970c51812dc3a010c7d01b50e0d17dc79c800000000000000000000000000000000000000000000000000000000";
-    const fakeHash = "0x83b7924e0ee35cf673adeaf9a774ee6918f3f5bd7c605369366a79dd8f1ec486";
-
+    const fakeHash = utils.keccak256(
+      utils.defaultAbiCoder.encode(["uint256", "uint256", "bytes"], [ticketID, blockNumber, fakeData])
+    );
 
     const tx5 = await fastBridgeReceiver.connect(bridger).claim(ticketID, fakeHash, { value: ONE_TENTH_ETH });
     let blockNumBefore = await ethers.provider.getBlockNumber();
     let blockBefore = await ethers.provider.getBlock(blockNumBefore);
     let timestampBefore = blockBefore.timestamp;
+    console.log("Block: %d", blockNumBefore);
     expect(tx5).to.emit(fastBridgeReceiver, "ClaimReceived").withArgs(ticketID, fakeHash, timestampBefore);
 
     // Challenger tx starts
@@ -433,30 +447,37 @@ describe("Demo pre-alpha1", function () {
     blockNumBefore = await ethers.provider.getBlockNumber();
     blockBefore = await ethers.provider.getBlock(blockNumBefore);
     timestampBefore = blockBefore.timestamp;
-
+    console.log("Block: %d", blockNumBefore);
     expect(tx6).to.emit(fastBridgeReceiver, "ClaimChallenged").withArgs(ticketID, timestampBefore);
 
-    //wait for challenge period to pass
+    // wait for challenge period to pass
     await network.provider.send("evm_increaseTime", [300]);
     await network.provider.send("evm_mine");
 
-
-    await expect(fastBridgeReceiver.connect(bridger).verifyAndRelay(ticketID, blockNumber, fakeData)).to.be.revertedWith('Claim is challenged');
+    await expect(
+      fastBridgeReceiver.connect(bridger).verifyAndRelay(ticketID, blockNumber, fakeData)
+    ).to.be.revertedWith("Claim is challenged");
     await fastBridgeSender.set_arb(arbsys.address);
 
     let data = await ethers.utils.defaultAbiCoder.decode(["address", "bytes"], fakeData);
 
-    await expect(fastBridgeSender.connect(bridger).sendSafeFallbackMock(ticketID, foreignGateway.address, data[1], { gasLimit: 1000000 })).to.be.revertedWith('Invalid message for ticketID.');
+    await expect(
+      fastBridgeSender
+        .connect(bridger)
+        .sendSafeFallbackMock(ticketID, foreignGateway.address, data[1], { gasLimit: 1000000 })
+    ).to.be.revertedWith("Invalid message for ticketID.");
 
     data = await ethers.utils.defaultAbiCoder.decode(["address", "bytes"], messageData);
-    let tx8 = await fastBridgeSender.connect(bridger).sendSafeFallbackMock(ticketID, foreignGateway.address, data[1], { gasLimit: 1000000 }
-    );
+    const tx8 = await fastBridgeSender
+      .connect(bridger)
+      .sendSafeFallbackMock(ticketID, foreignGateway.address, data[1], { gasLimit: 1000000 });
     expect(tx8).to.emit(fastBridgeSender, "L2ToL1TxCreated");
     expect(tx8).to.emit(arbitrable, "Ruling");
 
-    await expect(fastBridgeReceiver.withdrawClaimDeposit(ticketID)).to.be.revertedWith('Claim not verified: deposit forfeited');
+    await expect(fastBridgeReceiver.withdrawClaimDeposit(ticketID)).to.be.revertedWith(
+      "Claim not verified: deposit forfeited"
+    );
     await fastBridgeReceiver.withdrawChallengeDeposit(ticketID);
-
   });
 });
 
