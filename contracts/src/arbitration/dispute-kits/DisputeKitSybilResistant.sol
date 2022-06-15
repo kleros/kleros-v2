8f592cc7ca1e10dc8fc3e483bb3b6d04a11aeaf5
shotaronowhere <shawtarohgn@gmail.com>
2022-06-15 13:20:40 +0100

    feat(dk): sybil resistant dk efficiency

--- contracts/src/arbitration/dispute-kits/DisputeKitSybilResistant.sol
+++ contracts/src/arbitration/dispute-kits/DisputeKitSybilResistant.sol
@@ -249,18 +249,19 @@ contract DisputeKitSybilResistant is BaseDisputeKit, IEvidence {
         bytes32 key = bytes32(core.getSubcourtID(_coreDisputeID)); // Get the ID of the tree.
         uint256 drawnNumber = uint256(keccak256(abi.encode(getRandomNumber(), _coreDisputeID, round.votes.length)));
 
-        (uint256 K, , uint256[] memory nodes) = core.getSortitionSumTree(key);
+        uint256 K = core.getSortitionSumTreeK(key);
+        uint256 nodesLength = core.getSortitionSumTreeNodesLength(key);
         uint256 treeIndex = 0;
-        uint256 currentDrawnNumber = drawnNumber % nodes[0];
+        uint256 currentDrawnNumber = drawnNumber % core.getSortitionSumTreeNode(key, 0);
 
         // TODO: Handle the situation when no one has staked yet.
 
         // While it still has children
-        while ((K * treeIndex) + 1 < nodes.length) {
+        while ((K * treeIndex) + 1 < nodesLength) {
             for (uint256 i = 1; i <= K; i++) {
                 // Loop over children.
                 uint256 nodeIndex = (K * treeIndex) + i;
-                uint256 nodeValue = nodes[nodeIndex];
+                uint256 nodeValue = core.getSortitionSumTreeNode(key, nodeIndex);
 
                 if (currentDrawnNumber >= nodeValue) {
                     // Go to the next child.
