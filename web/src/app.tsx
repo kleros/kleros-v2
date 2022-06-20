65db988e6295e5c7f05dfb9451af2c839495425e
alcercu <alcercu@alumni.upv.es>
2022-06-20 12:55:45 +0200

    chore: rename pages/home to pages/Home

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -4,7 +4,7 @@ import { SWRConfig } from "swr";
 import { request } from "graphql-request";
 import { Routes, Route } from "react-router-dom";
 import Layout from "layout/index";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 
 const fetcherBuilder =
   (url: string) =>
