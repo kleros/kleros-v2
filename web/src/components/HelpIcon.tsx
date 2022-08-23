582c47c785376beeb1ce77e88b54ff4a60d606c7
alcercu <alcercu@alumni.upv.es>
2022-08-23 12:12:40 +0200

    feat(web): add dashboard page

--- web/src/components/HelpIcon.tsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-import _HelpIcon from "assets/svgs/menu-icons/help.svg";
-
-const HelpIcon = styled(_HelpIcon)`
-  fill: ${({ theme }) => theme.secondaryText};
-  margin: 4px;
-`;
-
-export default HelpIcon;
