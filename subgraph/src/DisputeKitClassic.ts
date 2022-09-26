5f455dc14a7de4cfaa4306e0fa1deaab459a04a3
alcercu <alcercu@alumni.upv.es>
2022-09-26 19:02:45 +0200

    fix(subgraph): remove safety check as is not needed

--- subgraph/src/DisputeKitClassic.ts
+++ subgraph/src/DisputeKitClassic.ts
@@ -3,21 +3,16 @@ import { Evidence as EvidenceEvent } from "../generated/DisputeKitClassic/Disput
 import { Evidence, EvidenceGroup } from "../generated/schema";
 
 export function handleEvidenceEvent(event: EvidenceEvent): void {
-  if (
-    event.params._arbitrator.toString() ===
-    "0x815d709EFCF5E69e2e9E2F8d3815d762496a2f0F"
-  ) {
-    const evidenceGroupID = event.params._evidenceGroupID;
-    let evidenceGroup = EvidenceGroup.load(evidenceGroupID.toString());
-    if (!evidenceGroup) {
-      evidenceGroup = new EvidenceGroup(evidenceGroupID.toString());
-      evidenceGroup.lastEvidenceID = BigInt.fromI32(0);
-      evidenceGroup.save();
-    }
-    const evidenceID = evidenceGroup.lastEvidenceID.plus(BigInt.fromI32(1));
-    const evidence = new Evidence(`${evidenceGroupID}-${evidenceID}`);
-    evidence.evidence = event.params._evidence;
-    evidence.evidenceGroup = evidenceGroupID.toString();
-    evidence.save();
+  const evidenceGroupID = event.params._evidenceGroupID;
+  let evidenceGroup = EvidenceGroup.load(evidenceGroupID.toString());
+  if (!evidenceGroup) {
+    evidenceGroup = new EvidenceGroup(evidenceGroupID.toString());
+    evidenceGroup.lastEvidenceID = BigInt.fromI32(0);
+    evidenceGroup.save();
   }
+  const evidenceID = evidenceGroup.lastEvidenceID.plus(BigInt.fromI32(1));
+  const evidence = new Evidence(`${evidenceGroupID}-${evidenceID}`);
+  evidence.evidence = event.params._evidence;
+  evidence.evidenceGroup = evidenceGroupID.toString();
+  evidence.save();
 }
