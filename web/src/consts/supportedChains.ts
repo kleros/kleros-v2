dd59b44b1439d9e9c4864cec5b0fc5f0473298fe
jaybuidl <jb@kleros.io>
2023-01-05 16:13:21 +0000

    chore: web migrated to Arbitrum Goerli

--- web/src/consts/supportedChains.ts
+++ web/src/consts/supportedChains.ts
@@ -1,11 +1,13 @@
 export const SUPPORTED_CHAINS = {
-  421611: {
-    chainName: "Arbitrum Rinkeby",
+  421613: {
+    chainName: "Arbitrum Goerli",
     nativeCurrency: { name: "ETH", symbol: "ETH", decimals: 18 },
-    rpcUrls: ["https://rinkeby.arbitrum.io/rpc"],
-    blockExplorerUrls: ["https://testnet.arbiscan.io/"],
+    rpcUrls: ["https://goerli-rollup.arbitrum.io/rpc"],
+    blockExplorerUrls: ["https://goerli.arbiscan.io/"],
   },
 };
+
+export const DEFAULT_CHAIN = 421613;
 
 export const SUPPORTED_CHAINIDS = Object.keys(SUPPORTED_CHAINS).map((x) =>
   parseInt(x)
