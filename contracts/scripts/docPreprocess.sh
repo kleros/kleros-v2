2923960d9c1e391e7b16af77365b46e979d65345
jaybuidl <jb@kleros.io>
2023-01-19 02:44:20 +0000

    fix: sed POSIX compatibility

--- contracts/scripts/docPreprocess.sh
+++ contracts/scripts/docPreprocess.sh
@@ -6,9 +6,9 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # Known issue: https://github.com/foundry-rs/foundry/issues/4118
 for tag in authors reviewers auditors bounties deployments tools
 do
-    find $SCRIPT_DIR/../src/ -type f -name '*.sol' | xargs sed -i '' "/^.*@$tag.*$/d"
+    find $SCRIPT_DIR/../src/ -type f -name '*.sol' | xargs sed -i.bak "/^.*@$tag.*$/d"
 done
 
-echo "Use 'git restore src/' to revert the changes"
+echo "Use 'git restore src/ && rm src/**/*.sol.bak' to revert the changes"
 
 
