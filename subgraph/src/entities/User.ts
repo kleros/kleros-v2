beedffaaa324e0008382d548a04aeb6e0b35556d
alcercu <alcercu@alumni.upv.es>
2023-01-23 11:41:09 +0100

    fix(subgraph): set non-nullable field totalStake when creating user

--- subgraph/src/entities/User.ts
+++ subgraph/src/entities/User.ts
@@ -1,4 +1,5 @@
 import { User } from "../../generated/schema";
+import { ZERO } from "../utils";
 
 export function ensureUser(id: string): User {
   let user = User.load(id);
@@ -12,6 +13,7 @@ export function ensureUser(id: string): User {
 
 export function createUserFromAddress(id: string): User {
   const user = new User(id);
+  user.totalStake = ZERO;
   user.save();
 
   return user;
