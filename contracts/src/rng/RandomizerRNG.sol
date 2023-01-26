f069ceddc543ca75f126f6e3ff0f034917a20278
jaybuidl <jb@kleros.io>
2023-01-26 15:21:37 +0000

    fix: moved to the new Randomizer contract, the callback takes now a uint256 instead of uint128

--- contracts/src/rng/RandomizerRNG.sol
+++ contracts/src/rng/RandomizerRNG.sol
@@ -10,12 +10,16 @@ import "./IRandomizer.sol";
  *  https://randomizer.ai/
  */
 contract RandomizerRNG is RNG {
-    uint256 public constant CALLBACK_GAS_LIMIT = 50000;
     address public governor; // The address that can withdraw funds.
+    uint256 public callbackGasLimit = 50000; // Gas limit for the randomizer callback
 
     IRandomizer public randomizer; // Randomizer address.
-    mapping(uint128 => uint256) public randomNumbers; // randomNumbers[requestID] is the random number for this request id, 0 otherwise.
-    mapping(address => uint128) public requesterToID; // Maps the requester to his latest request ID.
+    mapping(uint256 => uint256) public randomNumbers; // randomNumbers[requestID] is the random number for this request id, 0 otherwise.
+    mapping(address => uint256) public requesterToID; // Maps the requester to his latest request ID.
+
+    // ************************************* //
+    // *        Function Modifiers         * //
+    // ************************************* //
 
     modifier onlyByGovernor() {
         require(governor == msg.sender, "Governor only");
@@ -31,6 +35,10 @@ contract RandomizerRNG is RNG {
         governor = _governor;
     }
 
+    // ************************ //
+    // *      Governance      * //
+    // ************************ //
+
     /** @dev Changes the governor of the contract.
      *  @param _governor The new governor.
      */
@@ -38,30 +46,18 @@ contract RandomizerRNG is RNG {
         governor = _governor;
     }
 
-    /**
-     *  @dev Request a random number. The id of the request is tied to the sender.
+    /** @dev Change the Randomizer callback gas limit.
+     *  @param _callbackGasLimit the new limit.
      */
-    function requestRandomness(uint256 /*_block*/) external override {
-        uint128 id = uint128(randomizer.request(CALLBACK_GAS_LIMIT));
-        requesterToID[msg.sender] = id;
+    function setCallbackGasLimit(uint256 _callbackGasLimit) external onlyByGovernor {
+        callbackGasLimit = _callbackGasLimit;
     }
 
-    /**
-     *  @dev Return the random number.
-     *  @return randomNumber The random number or 0 if it is not ready or has not been requested.
+    /** @dev Change the Randomizer address.
+     *  @param _randomizer the new Randomizer address.
      */
-    function receiveRandomness(uint256 /*_block*/) external view override returns (uint256 randomNumber) {
-        // Get the latest request ID for this requester.
-        uint128 id = requesterToID[msg.sender];
-        randomNumber = randomNumbers[id];
-    }
-
-    /**
-     *  @dev Callback function called by the randomizer contract when the random value is generated.
-     */
-    function randomizerCallback(uint128 _id, bytes32 _value) external {
-        require(msg.sender == address(randomizer), "Caller not Randomizer");
-        randomNumbers[_id] = uint256(_value);
+    function setRandomizer(address _randomizer) external onlyByGovernor {
+        randomizer = IRandomizer(_randomizer);
     }
 
     /**
@@ -70,5 +66,39 @@ contract RandomizerRNG is RNG {
      */
     function randomizerWithdraw(uint256 _amount) external onlyByGovernor {
         randomizer.clientWithdrawTo(msg.sender, _amount);
+    }
+
+    // ************************************* //
+    // *         State Modifiers           * //
+    // ************************************* //
+
+    /**
+     *  @dev Request a random number. The id of the request is tied to the sender.
+     */
+    function requestRandomness(uint256 /*_block*/) external override {
+        uint256 id = randomizer.request(callbackGasLimit);
+        requesterToID[msg.sender] = id;
+    }
+
+    /**
+     *  @dev Callback function called by the randomizer contract when the random value is generated.
+     */
+    function randomizerCallback(uint256 _id, bytes32 _value) external {
+        require(msg.sender == address(randomizer), "Randomizer only");
+        randomNumbers[_id] = uint256(_value);
+    }
+
+    // ************************************* //
+    // *           Public Views            * //
+    // ************************************* //
+
+    /**
+     *  @dev Return the random number.
+     *  @return randomNumber The random number or 0 if it is not ready or has not been requested.
+     */
+    function receiveRandomness(uint256 /*_block*/) external view override returns (uint256 randomNumber) {
+        // Get the latest request ID for this requester.
+        uint256 id = requesterToID[msg.sender];
+        randomNumber = randomNumbers[id];
     }
 }
