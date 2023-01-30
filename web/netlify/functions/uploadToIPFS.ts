3df3daed4326a548ce39c5d0680b7ff4e88060fd
alcercu <alcercu@alumni.upv.es>
2023-01-30 17:18:16 +0100

    refactor(web): ready to merge

--- web/netlify/functions/uploadToIPFS.ts
+++ web/netlify/functions/uploadToIPFS.ts
@@ -1,8 +1,7 @@
-/* eslint-disable import/no-anonymous-default-export */
 import { Handler } from "@netlify/functions";
 import fetch from "node-fetch";
 
-const ESTUARI_API_KEY = "";
+const ESTUARI_API_KEY = process.env["ESTUARY_API_KEY"];
 const ESTUARI_URL = "https://api.estuary.tech/content/add";
 
 export const handler: Handler = async (event, context) => {
@@ -10,7 +9,6 @@ export const handler: Handler = async (event, context) => {
   if (event.body) {
     const newHeaders = event.headers;
     delete newHeaders.host;
-    console.log(event);
     const response = await fetch(ESTUARI_URL, {
       method: "POST",
       headers: {
@@ -20,54 +18,14 @@ export const handler: Handler = async (event, context) => {
       body: Buffer.from(event.body, "base64"),
     });
 
-    console.log(JSON.stringify(await response.json()));
-    // body: JSON.stringify({
-    //   jsonrpc: "2.0",
-    //   method: "eth_blockNumber",
-    //   params: [],
-    //   id: 1,
-    // }),
-    // });
+    const parsedResponse = await response.json();
+    return {
+      statusCode: response.status,
+      body: JSON.stringify(parsedResponse),
+    };
   }
   return {
-    statusCode: 200,
-    body: "hey",
+    statusCode: 500,
+    body: JSON.stringify({ message: "Invalid body format" }),
   };
 };
-
-// function parseMultipartForm(event: HandlerEvent) {
-//   return new Promise((resolve) => {
-//     const fields = {};
-//     const bb = busboy({ headers: event.headers });
-
-//     bb.on(
-//       "file",
-//       (
-//         fieldname: string,
-//         filestream: any,
-//         filename: string,
-//         _: any,
-//         mimeType: string
-//       ) => {
-//         console.log("typeof filestream", typeof filestream);
-//         filestream.on("data", (data) => {
-//           console.log("typeof data", typeof data);
-//           fields[fieldname] = {
-//             filename,
-//             type: mimeType,
-//             content: data,
-//           };
-//         });
-//       }
-//     );
-
-//     bb.on("field", (fieldName: string, value: any) => {
-//       console.log(fieldName, value);
-//       fields[fieldName] = value;
-//     });
-
-//     console.log("event.body: ", event.body);
-//     console.log("listener count: ", bb.listenerCount("field"));
-//     bb.write(event.body, () => resolve(fields));
-//   });
-// }
