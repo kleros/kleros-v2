22d5f2062a5cf8cac302cb384361118961d524ee
jaybuidl <jb@kleros.io>
2023-02-15 21:37:35 +0000

    feat: sentry

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,8 +1,21 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
 import App from "./app";
 import Modal from "react-modal";
 import { HashRouter as Router } from "react-router-dom";
+
+Sentry.init({
+  dsn: process.env.REACT_APP_SENTRY_ENDPOINT,
+  environment: process.env.REACT_APP_CONTEXT,
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 const container = document.getElementById("app");
 Modal.setAppElement(container!);
