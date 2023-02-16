e1c0144c0ff6ef2e94678da8efaa0b6ef60dae9e
jaybuidl <jb@kleros.io>
2023-02-16 15:31:51 +0000

    chore: tweak sentry version number

--- web/scripts/sentry-release.sh
+++ web/scripts/sentry-release.sh
@@ -4,14 +4,14 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cd $SCRIPT_DIR/..
 
-VERSION=$(sentry-cli releases propose-version)
-sentry-cli releases new "$VERSION"
-sentry-cli releases set-commits "$VERSION" --auto
+version="court-v2@v$(cat package.json | jq -r .version)-$(git rev-parse --short HEAD)"
+sentry-cli releases new "$version"
+sentry-cli releases set-commits "$version" --auto
 
 rm -rf dist/
 yarn build
-sentry-cli releases files $VERSION upload-sourcemaps dist/
+sentry-cli releases files $version upload-sourcemaps dist/
 
-sentry-cli releases finalize "$VERSION"
+sentry-cli releases finalize "$version"
 
 cd -
