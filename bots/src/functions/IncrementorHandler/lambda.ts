8a266600deff9954b6d4f7fa405bbb5b32a2f4a8
jaybuidl <jb@kleros.io>
2023-03-02 00:25:01 +0000

    chore: logging to logtail via pino

--- bots/src/functions/IncrementorHandler/lambda.ts
+++ bots/src/functions/IncrementorHandler/lambda.ts
@@ -1,4 +1,15 @@
 import { SQSHandler, SQSEvent, Context, SQSBatchResponse } from "aws-lambda";
+import pino from "pino";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const transport = pino.transport({
+  target: "@logtail/pino",
+  options: { sourceToken: process.env.LOGTAIL_SOURCE_TOKEN },
+});
+
+const logger = pino(transport);
 
 /**
  * Event doc: https://docs.aws.amazon.com/lambda/latest/dg/with-sqs.html
@@ -10,8 +21,12 @@ import { SQSHandler, SQSEvent, Context, SQSBatchResponse } from "aws-lambda";
  * Return doc: https://docs.aws.amazon.com/lambda/latest/dg/with-sqs.html#services-sqs-batchfailurereporting
  * @returns {SQSBatchResponse} object - SQS batch response
  */
-export const handler: SQSHandler = async (event: SQSEvent, context: Context) => {
-  console.log("Executing lambda handler on event: %O", event.Records);
+export const handler: SQSHandler = async (
+  event: SQSEvent,
+  context: Context
+) => {
+  logger.info(event.Records, "Executing lambda handler on event");
+
   const response: SQSBatchResponse = {
     batchItemFailures: [
       {
