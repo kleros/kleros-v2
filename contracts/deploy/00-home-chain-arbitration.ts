a7030646d72e620a6dc4be21898fcd19404d922c
jaybuidl <jb@kleros.io>
2023-04-21 11:59:52 +0100

    chore: randomizer address update, removed old artifact

--- contracts/deploy/00-home-chain-arbitration.ts
+++ contracts/deploy/00-home-chain-arbitration.ts
@@ -13,21 +13,30 @@ const pnkByChain = new Map<HomeChains, string>([
   [HomeChains.ARBITRUM_GOERLI, "0x4DEeeFD054434bf6721eF39Aa18EfB3fd0D12610"],
 ]);
 
+// https://randomizer.ai/docs#addresses
 const randomizerByChain = new Map<HomeChains, string>([
-  [HomeChains.ARBITRUM_ONE, "0x00"],
-  [HomeChains.ARBITRUM_GOERLI, "0xF25c6Ad3694dA9D3C97F4D316b0B31F96cEb39d5"],
+  [HomeChains.ARBITRUM_ONE, "0x5b8bB80f2d72D0C85caB8fB169e8170A05C94bAF"],
+  [HomeChains.ARBITRUM_GOERLI, "0x923096Da90a3b60eb7E12723fA2E1547BA9236Bc"],
 ]);
 
-const deployArbitration: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
+const deployArbitration: DeployFunction = async (
+  hre: HardhatRuntimeEnvironment
+) => {
   const { deployments, getNamedAccounts, getChainId } = hre;
   const { deploy, execute } = deployments;
   const { AddressZero } = hre.ethers.constants;
   const RNG_LOOKAHEAD = 20;
 
   // fallback to hardhat node signers on local network
-  const deployer = (await getNamedAccounts()).deployer ?? (await hre.ethers.getSigners())[0].address;
+  const deployer =
+    (await getNamedAccounts()).deployer ??
+    (await hre.ethers.getSigners())[0].address;
   const chainId = Number(await getChainId());
-  console.log("Deploying to %s with deployer %s", HomeChains[chainId], deployer);
+  console.log(
+    "Deploying to %s with deployer %s",
+    HomeChains[chainId],
+    deployer
+  );
 
   if (chainId === HomeChains.HARDHAT) {
     pnkByChain.set(
@@ -57,7 +66,8 @@ const deployArbitration: DeployFunction = async (hre: HardhatRuntimeEnvironment)
     log: true,
   });
 
-  const randomizer = randomizerByChain.get(Number(await getChainId())) ?? AddressZero;
+  const randomizer =
+    randomizerByChain.get(Number(await getChainId())) ?? AddressZero;
   const rng = await deploy("RandomizerRNG", {
     from: deployer,
     args: [randomizer, deployer],
@@ -100,9 +110,16 @@ const deployArbitration: DeployFunction = async (hre: HardhatRuntimeEnvironment)
   });
 
   // execute DisputeKitClassic.changeCore() only if necessary
-  const currentCore = await hre.ethers.getContractAt("DisputeKitClassic", disputeKit.address).then((dk) => dk.core());
+  const currentCore = await hre.ethers
+    .getContractAt("DisputeKitClassic", disputeKit.address)
+    .then((dk) => dk.core());
   if (currentCore !== klerosCore.address) {
-    await execute("DisputeKitClassic", { from: deployer, log: true }, "changeCore", klerosCore.address);
+    await execute(
+      "DisputeKitClassic",
+      { from: deployer, log: true },
+      "changeCore",
+      klerosCore.address
+    );
   }
 
   await deploy("DisputeResolver", {
