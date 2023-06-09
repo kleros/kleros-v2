09e8890f8b1d545693f60ead1ad4fffdad5cdd94
nhestrompia <nhestrompia@gmail.com>
2023-06-09 11:46:55 +0300

    fix: provider error

--- web/src/context/Web3Provider.tsx
+++ web/src/context/Web3Provider.tsx
@@ -4,11 +4,12 @@ import { alchemyProvider } from "@wagmi/core/providers/alchemy";
 import { Web3Modal } from "@web3modal/react";
 import { configureChains, createConfig, WagmiConfig } from "wagmi";
 import { arbitrumGoerli, gnosisChiado } from "wagmi/chains";
+import { publicProvider } from "wagmi/providers/public";
 
 const chains = [arbitrumGoerli, gnosisChiado];
 const projectId = "6efaa26765fa742153baf9281e218217";
 
-const { publicClient } = configureChains(chains, [alchemyProvider({ apiKey: "" })]);
+const { publicClient } = configureChains(chains, [alchemyProvider({ apiKey: "" }), publicProvider()]);
 
 const wagmiConfig = createConfig({
   autoConnect: false,
