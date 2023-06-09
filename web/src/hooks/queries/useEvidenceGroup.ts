3f637bbb4ba9fb021b46d57db6ad7b1d4112f58b
nhestrompia <nhestrompia@gmail.com>
2023-06-09 11:00:36 +0300

    refactor: migrate hooks to viem/wagmi

--- web/src/hooks/queries/useEvidenceGroup.ts
+++ web/src/hooks/queries/useEvidenceGroup.ts
@@ -1,31 +1,46 @@
 import useSWRImmutable from "swr/immutable";
-// import { utils } from "ethers";
-import { BigNumber } from "ethers";
-import { useIMetaEvidence } from "hooks/contracts/generated";
+import { getIMetaEvidence, iMetaEvidenceABI } from "hooks/contracts/generated";
+import { usePublicClient } from "wagmi";
+import { parseAbiItem } from "viem";
 
-export const useEvidenceGroup = (
-  disputeID?: string,
-  arbitrableAddress?: string
-) => {
+export const useEvidenceGroup = (disputeID?: string, arbitrableAddress?: string) => {
   // const formattedAddress = arbitrableAddress
   //   ? utils.getAddress(arbitrableAddress)
   //   : undefined;
-  const arbitrable = useIMetaEvidence({
+  const publicClient = usePublicClient();
+
+  const arbitrable = getIMetaEvidence({
     address: "0xc0fcc96BFd78e36550FCaB434A9EE1210B57225b",
   });
+  console.log("🚀 ~ file: useEvidenceGroup.ts:14 ~ useEvidenceGroup ~ arbitrable:", arbitrable);
+  console.log("_disputeID", disputeID);
+
   return useSWRImmutable(
-    () =>
-      arbitrable ? `EvidenceGroup${disputeID}${arbitrableAddress}` : false,
+    () => (arbitrable ? `EvidenceGroup${disputeID}${arbitrableAddress}` : false),
     async () => {
       if (arbitrable) {
-        const disputeFilter = arbitrable.filters.Dispute(
-          null,
-          BigNumber.from(parseInt(disputeID!) + 1),
-          null,
-          null
-        );
-        const disputeEvents = await arbitrable.queryFilter(disputeFilter);
-        return disputeEvents[0].args?._evidenceGroupID.toString();
+        const disputeFilter = await arbitrable.createEventFilter.Dispute({
+          _disputeID: BigInt(parseInt(disputeID!) + 1),
+        });
+        // const disputeFilter = await publicClient.createContractEventFilter({
+        //   abi: iMetaEvidenceABI,
+        //   address: "0xc0fcc96BFd78e36550FCaB434A9EE1210B57225b",
+        //   eventName: "Dispute",
+        //   args: {
+        //     _disputeID: BigInt(parseInt(disputeID!) + 1),
+        //   },
+        // });
+        // const disputeFilter = await publicClient.createEventFilter({
+        //   address: "0xc0fcc96BFd78e36550FCaB434A9EE1210B57225b",
+        //   event: parseAbiItem('event Dispute(IArbitrator indexed _arbitrator, uint256 indexed _disputeID, uint256 _metaEvidenceID, uint256 _evidenceGroupID)'),
+        //   args:{_disputeId: BigInt(parseInt(disputeID!) + 1)
+        //   }
+        // });
+
+        const disputeEvents = await publicClient.getFilterLogs({
+          filter: disputeFilter,
+        });
+        return disputeEvents[0];
       } else throw Error;
     }
   );
