c47ecbda6b0aeaf50a6bb5abe9ae019a90d0ab6c
nhestrompia <nhestrompia@gmail.com>
2023-06-12 20:04:05 +0300

    fix: another bigint notation fix

--- web/src/hooks/useParsedAmount.ts
+++ web/src/hooks/useParsedAmount.ts
@@ -2,5 +2,5 @@ import { useMemo } from "react";
 import { parseUnits } from "viem";
 
 export function useParsedAmount(amount: string): bigint {
-  return useMemo(() => (amount === "" ? BigInt(0) : parseUnits(`${parseInt(amount)}`, 18)), [amount]);
+  return useMemo(() => (amount === "" ? 0n : parseUnits(`${parseInt(amount)}`, 18)), [amount]);
 }
