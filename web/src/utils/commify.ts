4d8a007bbed942209fb03bc47fe4be4260e94127
marino <102478601+kemuru@users.noreply.github.com>
2023-06-12 11:20:11 +0100

    fix: sonarcloud error commify.ts

--- web/src/utils/commify.ts
+++ web/src/utils/commify.ts
@@ -1,7 +1,7 @@
 export const commify = (value: number | bigint | string): string => {
   const [integerPart, decimalPart] = value.toString().split(".");
 
-  const formattedIntegerPart = integerPart.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  const formattedIntegerPart = integerPart.replace(/\d(?=(\d{3})+$)/g, "$&,");
 
   return decimalPart ? `${formattedIntegerPart}.${decimalPart}` : formattedIntegerPart;
 };
