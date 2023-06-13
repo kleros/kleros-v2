4f3abc56af0f5150bcf50f390716dfbd3860be5a
alcercu <333aleix333@gmail.com>
2023-06-13 13:09:03 +0200

    fix(web): use ethers commify

--- web/src/utils/commify.ts
+++ web/src/utils/commify.ts
@@ -1,16 +1,52 @@
-export const commify = (value: number | bigint | string): string => {
-  const [integerPart, decimalPart] = value.toString().split(".");
+export function commify(value: string | number): string {
+  const comps = String(value).split(".");
 
-  let formattedIntegerPart = "";
-  let counter = 0;
+  if (
+    comps.length > 2 ||
+    !comps[0].match(/^-?[0-9]*$/) ||
+    (comps[1] && !comps[1].match(/^[0-9]*$/)) ||
+    value === "." ||
+    value === "-."
+  ) {
+    return value.toString();
+  }
 
-  for (let i = integerPart.length - 1; i >= 0; i--) {
-    counter++;
-    formattedIntegerPart = integerPart[i] + formattedIntegerPart;
-    if (counter % 3 === 0 && i !== 0) {
-      formattedIntegerPart = "," + formattedIntegerPart;
+  // Make sure we have at least one whole digit (0 if none)
+  let whole = comps[0];
+
+  let negative = "";
+  if (whole.substring(0, 1) === "-") {
+    negative = "-";
+    whole = whole.substring(1);
+  }
+
+  // Make sure we have at least 1 whole digit with no leading zeros
+  while (whole.substring(0, 1) === "0") {
+    whole = whole.substring(1);
+  }
+  if (whole === "") {
+    whole = "0";
+  }
+
+  let suffix = "";
+  if (comps.length === 2) {
+    suffix = "." + (comps[1] || "0");
+  }
+  while (suffix.length > 2 && suffix[suffix.length - 1] === "0") {
+    suffix = suffix.substring(0, suffix.length - 1);
+  }
+
+  const formatted: string[] = [];
+  while (whole.length) {
+    if (whole.length <= 3) {
+      formatted.unshift(whole);
+      break;
+    } else {
+      const index = whole.length - 3;
+      formatted.unshift(whole.substring(index));
+      whole = whole.substring(0, index);
     }
   }
 
-  return decimalPart ? `${formattedIntegerPart}.${decimalPart}` : formattedIntegerPart;
-};
+  return negative + formatted.join(",") + suffix;
+}
