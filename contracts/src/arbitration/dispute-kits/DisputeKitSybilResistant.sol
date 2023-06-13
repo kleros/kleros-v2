76eabb7a024d519f0cc91b8a230953d0de963389
jaybuidl <jb@kleros.io>
2023-06-13 15:42:32 +0100

    feat: interfaces wip

--- contracts/src/arbitration/dispute-kits/DisputeKitSybilResistant.sol
+++ contracts/src/arbitration/dispute-kits/DisputeKitSybilResistant.sol
@@ -395,11 +395,11 @@ contract DisputeKitSybilResistant is BaseDisputeKit, IEvidence {
         }
     }
 
-    /// @dev Submits evidence.
-    /// @param _evidenceGroupID Unique identifier of the evidence group the evidence belongs to. It's the submitter responsability to submit the right evidence group ID.
+    /// @dev Submits evidence for a dispute.
+    /// @param _externalDisputeID Unique identifier for this dispute outside Kleros. It's the submitter responsability to submit the right evidence group ID.
     /// @param _evidence IPFS path to evidence, example: '/ipfs/Qmarwkf7C9RuzDEJNnarT3WZ7kem5bk8DZAzx78acJjMFH/evidence.json'.
-    function submitEvidence(uint256 _evidenceGroupID, string calldata _evidence) external {
-        emit Evidence(_evidenceGroupID, msg.sender, _evidence);
+    function submitEvidence(uint256 _externalDisputeID, string calldata _evidence) external {
+        emit Evidence(_externalDisputeID, msg.sender, _evidence);
     }
 
     // ************************************* //
