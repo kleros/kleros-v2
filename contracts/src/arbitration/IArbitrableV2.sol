6089177b51ee6f1f1d88240b93ac7579f1c7f984
jaybuidl <jb@kleros.io>
2023-06-13 20:03:13 +0100

    fix: natspec

--- contracts/src/arbitration/IArbitrableV2.sol
+++ contracts/src/arbitration/IArbitrableV2.sol
@@ -4,34 +4,24 @@ pragma solidity ^0.8;
 
 import "./IArbitratorV2.sol";
 
-/**
- * @title IArbitrableV2
- * Arbitrable interface.
- * When developing arbitrable contracts, we need to:
- * - Define the action taken when a ruling is received by the contract.
- * - Allow dispute creation. For this a function must call arbitrator.createDispute{value: _fee}(_choices,_extraData);
- */
+/// @title IArbitrableV2
+/// @notice Arbitrable interface.
+/// When developing arbitrable contracts, we need to:
+/// - Define the action taken when a ruling is received by the contract.
+/// - Allow dispute creation. For this a function must call arbitrator.createDispute{value: _fee}(_choices,_extraData);
 interface IArbitrableV2 {
-    /**
-     * @dev To be emitted when a new dispute template is created.
-     * @param _templateId The ID of the dispute template.
-     * @param _templateTag An optional tag for the dispute template, such as "registration" or "removal".
-     * @param data The template data.
-     */
-    event NewDisputeTemplate(
-        uint256 indexed _templateId,
-        string indexed _templateTag,
-        string data
-    );
+    /// @dev To be emitted when a new dispute template is created.
+    /// @param _templateId The ID of the dispute template.
+    /// @param _templateTag An optional tag for the dispute template, such as "registration" or "removal".
+    /// @param data The template data.
+    event DisputeTemplate(uint256 indexed _templateId, string indexed _templateTag, string data);
 
-    /**
-     * @dev To be emitted when a dispute is created to link the correct meta-evidence to the disputeID.
-     * @param _arbitrator The arbitrator of the contract.
-     * @param _arbitrableDisputeID The ID of the dispute in the Arbitrable contract.
-     * @param _externalDisputeID An identifier created outside Kleros by the protocol requesting arbitration.
-     * @param _templateId The ID of the dispute template. Should not be used with _templateUri.
-     * @param _templateUri IPFS path to the dispute template starting with '/ipfs/'. Should not be used with _templateId.
-     */
+    /// @dev To be emitted when a dispute is created to link the correct meta-evidence to the disputeID.
+    /// @param _arbitrator The arbitrator of the contract.
+    /// @param _arbitrableDisputeID The ID of the dispute in the Arbitrable contract.
+    /// @param _externalDisputeID An identifier created outside Kleros by the protocol requesting arbitration.
+    /// @param _templateId The ID of the dispute template. Should not be used with _templateUri.
+    /// @param _templateUri IPFS path to the dispute template starting with '/ipfs/'. Should not be used with _templateId.
     event DisputeRequest(
         IArbitratorV2 indexed _arbitrator,
         uint256 indexed _arbitrableDisputeID,
@@ -39,40 +29,36 @@ interface IArbitrableV2 {
         uint256 _templateId,
         string _templateUri
     );
-    
-    /**
-     * @dev To be emitted when a dispute is created to link the correct meta-evidence to the disputeID.
-     * @param _arbitrator The arbitrator of the contract.
-     * @param _arbitrableChainId The chain ID of the Arbitrable contract.
-     * @param _arbitrable The address of the Arbitrable contract.
-     * @param _arbitrableDisputeID The ID of the dispute in the Arbitrable contract.
-     * @param _externalDisputeID An identifier created outside Kleros by the protocol requesting arbitration.
-     * @param _templateId The ID of the dispute template. Should not be used with _templateUri.
-     * @param _templateUri IPFS path to the dispute template starting with '/ipfs/'. Should not be used with _templateId.
-     */
+
+    /// @dev To be emitted when a dispute is created to link the correct meta-evidence to the disputeID.
+    /// @param _arbitrator The arbitrator of the contract.
+    /// @param _arbitrableChainId The chain ID of the Arbitrable contract.
+    /// @param _arbitrable The address of the Arbitrable contract.
+    /// @param _arbitrableDisputeID The ID of the dispute in the Arbitrable contract.
+    /// @param _externalDisputeID An identifier created outside Kleros by the protocol requesting arbitration.
+    /// @param _templateId The ID of the dispute template. Should not be used with _templateUri.
+    /// @param _templateUri IPFS path to the dispute template starting with '/ipfs/'. Should not be used with _templateId.
     event CrossChainDisputeRequest(
-        IArbitratorV2 indexed _arbitrator, 
+        IArbitratorV2 indexed _arbitrator,
         uint256 _arbitrableChainId,
-        address indexed _arbitrable, 
+        address indexed _arbitrable,
         uint256 indexed _arbitrableDisputeID,
         uint256 _externalDisputeID,
         uint256 _templateId,
         string _templateUri
     );
 
-    /**
-     * @dev To be raised when a ruling is given.
-     * @param _arbitrator The arbitrator giving the ruling.
-     * @param _disputeID ID of the dispute in the Arbitrator contract.
-     * @param _ruling The ruling which was given.
-     */
+    /// @dev To be raised when a ruling is given.
+    /// @param _arbitrator The arbitrator giving the ruling.
+    /// @param _disputeID ID of the dispute in the Arbitrator contract.
+    /// @param _ruling The ruling which was given.
     event Ruling(IArbitratorV2 indexed _arbitrator, uint256 indexed _disputeID, uint256 _ruling);
 
-    /**
-     * @dev Give a ruling for a dispute. Must be called by the arbitrator.
-     * The purpose of this function is to ensure that the address calling it has the right to rule on the contract.
-     * @param _disputeID ID of the dispute in the Arbitrator contract.
-     * @param _ruling Ruling given by the arbitrator. Note that 0 is reserved for "Not able/wanting to make a decision".
-     */
+    /// @dev Give a ruling for a dispute.
+    ///      Must be called by the arbitrator.
+    ///      The purpose of this function is to ensure that the address calling it has the right to rule on the contract.
+    /// @param _disputeID ID of the dispute in the Arbitrator contract.
+    /// @param _ruling Ruling given by the arbitrator.
+    /// Note that 0 is reserved for "Not able/wanting to make a decision".
     function rule(uint256 _disputeID, uint256 _ruling) external;
 }
--- contracts/src/arbitration/IArbitratorV2.sol
+++ contracts/src/arbitration/IArbitratorV2.sol
@@ -4,51 +4,42 @@ pragma solidity ^0.8;
 
 import "./IArbitrableV2.sol";
 
-/**
- * @title Arbitrator
- * Arbitrator interface that implements the new arbitration standard.
- * Unlike the ERC-792 this standard is not concerned with appeals, so each arbitrator can implement an appeal system that suits it the most.
- * When developing arbitrator contracts we need to:
- * - Define the functions for dispute creation (createDispute). Don't forget to store the arbitrated contract and the disputeID (which should be unique, may nbDisputes).
- * - Define the functions for cost display (arbitrationCost).
- * - Allow giving rulings. For this a function must call arbitrable.rule(disputeID, ruling).
- */
+/// @title Arbitrator
+/// Arbitrator interface that implements the new arbitration standard.
+/// Unlike the ERC-792 this standard is not concerned with appeals, so each arbitrator can implement an appeal system that suits it the most.
+/// When developing arbitrator contracts we need to:
+/// - Define the functions for dispute creation (createDispute). Don't forget to store the arbitrated contract and the disputeID (which should be unique, may nbDisputes).
+/// - Define the functions for cost display (arbitrationCost).
+/// - Allow giving rulings. For this a function must call arbitrable.rule(disputeID, ruling).
 interface IArbitratorV2 {
-    /**
-     * @dev To be emitted when a dispute is created.
-     * @param _disputeID Identifier of the dispute.
-     * @param _arbitrable The contract which created the dispute.
-     */
+    /// @dev To be emitted when a dispute is created.
+    /// @param _disputeID Identifier of the dispute.
+    /// @param _arbitrable The contract which created the dispute.
     event DisputeCreation(uint256 indexed _disputeID, IArbitrableV2 indexed _arbitrable);
 
-    /**
-     * @dev To be raised when a ruling is given.
-     * @param _arbitrable The arbitrable receiving the ruling.
-     * @param _disputeID Identifier of the dispute in the Arbitrator contract.
-     * @param _ruling The ruling which was given.
-     */
+    /// @dev To be raised when a ruling is given.
+    /// @param _arbitrable The arbitrable receiving the ruling.
+    /// @param _disputeID Identifier of the dispute in the Arbitrator contract.
+    /// @param _ruling The ruling which was given.
     event Ruling(IArbitrableV2 indexed _arbitrable, uint256 indexed _disputeID, uint256 _ruling);
 
-    /**
-     * @dev Create a dispute. Must be called by the arbitrable contract.
-     * Must pay at least arbitrationCost(_extraData).
-     * @param _choices Amount of choices the arbitrator can make in this dispute.
-     * @param _extraData Can be used to give additional info on the dispute to be created.
-     * @return disputeID Identifier of the dispute created.
-     */
+    /// @dev Create a dispute.
+    ///      Must be called by the arbitrable contract.
+    ///      Must pay at least arbitrationCost(_extraData).
+    /// @param _choices Amount of choices the arbitrator can make in this dispute.
+    /// @param _extraData Can be used to give additional info on the dispute to be created.
+    /// @return disputeID Identifier of the dispute created.
     function createDispute(uint256 _choices, bytes calldata _extraData) external payable returns (uint256 disputeID);
 
-    /**
-     * @dev Compute the cost of arbitration. It is recommended not to increase it often, as it can be highly time and gas consuming for the arbitrated contracts to cope with fee augmentation.
-     * @param _extraData Can be used to give additional info on the dispute to be created.
-     * @return cost Required cost of arbitration.
-     */
+    /// @dev Compute the cost of arbitration.
+    ///      It is recommended not to increase it often, as it can be highly time and gas consuming for the arbitrated contracts to cope with fee augmentation.
+    /// @param _extraData Can be used to give additional info on the dispute to be created.
+    /// @return cost Required cost of arbitration.
     function arbitrationCost(bytes calldata _extraData) external view returns (uint256 cost);
 
-    /**
-     *  @dev Return the current ruling of a dispute. This is useful for parties to know if they should appeal.
-     *  @param _disputeID ID of the dispute.
-     *  @return ruling The ruling which has been given or the one which will be given if there is no appeal.
-     */
+    /// @dev Return the current ruling of a dispute.
+    ///      This is useful for parties to know if they should appeal.
+    /// @param _disputeID ID of the dispute.
+    /// @return ruling The ruling which has been given or the one which will be given if there is no appeal.
     function currentRuling(uint _disputeID) external view returns (uint ruling);
 }
