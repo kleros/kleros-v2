21adc28e9b6da2e7a649efe1eef022d2f5432d3f
jaybuidl <jb@kleros.io>
2023-06-13 20:18:58 +0100

    feat: migrated DisputeResolver

--- contracts/src/arbitration/arbitrables/DisputeResolver.sol
+++ contracts/src/arbitration/arbitrables/DisputeResolver.sol
@@ -1,18 +1,17 @@
 // SPDX-License-Identifier: MIT
 
-/// @custom:authors: [@ferittuncer, @unknownunknown1]
+/// @custom:authors: [@ferittuncer, @unknownunknown1,@jaybuidl]
 /// @custom:reviewers: []
 /// @custom:auditors: []
 /// @custom:bounties: []
 
-import "../IArbitrable.sol";
-import "../../evidence/IMetaEvidence.sol";
+import "../IArbitrableV2.sol";
 
 pragma solidity 0.8.18;
 
 /// @title DisputeResolver
 /// DisputeResolver contract adapted for V2 https://github.com/kleros/arbitrable-proxy-contracts/blob/master/contracts/ArbitrableProxy.sol.
-contract DisputeResolver is IArbitrable, IMetaEvidence {
+contract DisputeResolver is IArbitrableV2 {
     struct DisputeStruct {
         bytes arbitratorExtraData; // Extra data for the dispute.
         bool isRuled; // True if the dispute has been ruled.
@@ -20,26 +19,26 @@ contract DisputeResolver is IArbitrable, IMetaEvidence {
         uint256 numberOfRulingOptions; // The number of choices the arbitrator can give.
     }
 
-    IArbitrator public immutable arbitrator; // Arbitrator is set in constructor and never changed.
+    IArbitratorV2 public immutable arbitrator; // Arbitrator is set in constructor and never changed.
 
     DisputeStruct[] public disputes; // Local disputes.
-    mapping(uint256 => uint256) public externalIDtoLocalID; // Maps external (arbitrator side) dispute IDs to local dispute IDs.
+    mapping(uint256 => uint256) public arbitratorDisputeIDToLocalID; // Maps external (arbitrator side) dispute IDs to local dispute IDs.
 
     /// @dev Constructor
     /// @param _arbitrator Target global arbitrator for any disputes.
-    constructor(IArbitrator _arbitrator) {
+    constructor(IArbitratorV2 _arbitrator) {
         arbitrator = _arbitrator;
     }
 
     /// @dev TRUSTED. Calls createDispute function of the specified arbitrator to create a dispute.
     /// Note that we don’t need to check that msg.value is enough to pay arbitration fees as it’s the responsibility of the arbitrator contract.
     /// @param _arbitratorExtraData Extra data for the arbitrator of the dispute.
-    /// @param _metaevidenceURI Link to metaevidence of the dispute.
+    /// @param _template Dispute template.
     /// @param _numberOfRulingOptions Number of ruling options.
     /// @return disputeID Dispute id (on arbitrator side) of the created dispute.
     function createDispute(
         bytes calldata _arbitratorExtraData,
-        string calldata _metaevidenceURI,
+        string calldata _template,
         uint256 _numberOfRulingOptions
     ) external payable returns (uint256 disputeID) {
         require(_numberOfRulingOptions > 1, "Should be at least 2 ruling options.");
@@ -55,17 +54,16 @@ contract DisputeResolver is IArbitrable, IMetaEvidence {
             })
         );
 
-        externalIDtoLocalID[disputeID] = localDisputeID;
-
-        emit MetaEvidence(localDisputeID, _metaevidenceURI);
-        emit Dispute(arbitrator, disputeID, localDisputeID, localDisputeID);
+        arbitratorDisputeIDToLocalID[disputeID] = localDisputeID;
+        emit DisputeTemplate(localDisputeID, "", _template);
+        emit DisputeRequest(arbitrator, disputeID, localDisputeID, localDisputeID, "");
     }
 
     /// @dev To be called by the arbitrator of the dispute, to declare the winning ruling.
     /// @param _externalDisputeID ID of the dispute in arbitrator contract.
     /// @param _ruling The ruling choice of the arbitration.
     function rule(uint256 _externalDisputeID, uint256 _ruling) external override {
-        uint256 localDisputeID = externalIDtoLocalID[_externalDisputeID];
+        uint256 localDisputeID = arbitratorDisputeIDToLocalID[_externalDisputeID];
         DisputeStruct storage dispute = disputes[localDisputeID];
         require(msg.sender == address(arbitrator), "Only the arbitrator can execute this.");
         require(_ruling <= dispute.numberOfRulingOptions, "Invalid ruling.");
@@ -74,6 +72,6 @@ contract DisputeResolver is IArbitrable, IMetaEvidence {
         dispute.isRuled = true;
         dispute.ruling = _ruling;
 
-        emit Ruling(IArbitrator(msg.sender), _externalDisputeID, dispute.ruling);
+        emit Ruling(IArbitratorV2(msg.sender), _externalDisputeID, dispute.ruling);
     }
 }
