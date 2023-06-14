77cb6eda5b2280ca1dd328e99fba776886e1a30c
jaybuidl <jb@kleros.io>
2023-06-14 01:28:03 +0100

    feat: resolver migrated and deployed to chiado, interfaces natspec improved

--- contracts/src/arbitration/IArbitratorV2.sol
+++ contracts/src/arbitration/IArbitratorV2.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8;
+pragma solidity 0.8.18;
 
 import "./IArbitrableV2.sol";
 
@@ -13,13 +13,13 @@ import "./IArbitrableV2.sol";
 /// - Allow giving rulings. For this a function must call arbitrable.rule(disputeID, ruling).
 interface IArbitratorV2 {
     /// @dev To be emitted when a dispute is created.
-    /// @param _disputeID Identifier of the dispute.
+    /// @param _disputeID The identifier of the dispute in the Arbitrator contract.
     /// @param _arbitrable The contract which created the dispute.
     event DisputeCreation(uint256 indexed _disputeID, IArbitrableV2 indexed _arbitrable);
 
     /// @dev To be raised when a ruling is given.
     /// @param _arbitrable The arbitrable receiving the ruling.
-    /// @param _disputeID Identifier of the dispute in the Arbitrator contract.
+    /// @param _disputeID The identifier of the dispute in the Arbitrator contract.
     /// @param _ruling The ruling which was given.
     event Ruling(IArbitrableV2 indexed _arbitrable, uint256 indexed _disputeID, uint256 _ruling);
 
@@ -28,7 +28,7 @@ interface IArbitratorV2 {
     ///      Must pay at least arbitrationCost(_extraData).
     /// @param _choices Amount of choices the arbitrator can make in this dispute.
     /// @param _extraData Can be used to give additional info on the dispute to be created.
-    /// @return disputeID Identifier of the dispute created.
+    /// @return disputeID The identifier of the dispute created.
     function createDispute(uint256 _choices, bytes calldata _extraData) external payable returns (uint256 disputeID);
 
     /// @dev Compute the cost of arbitration.
@@ -39,7 +39,7 @@ interface IArbitratorV2 {
 
     /// @dev Return the current ruling of a dispute.
     ///      This is useful for parties to know if they should appeal.
-    /// @param _disputeID ID of the dispute.
+    /// @param _disputeID The identifer of the dispute.
     /// @return ruling The ruling which has been given or the one which will be given if there is no appeal.
     function currentRuling(uint _disputeID) external view returns (uint ruling);
 }
