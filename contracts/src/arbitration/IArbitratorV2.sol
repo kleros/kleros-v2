8fa0abe7e5b3125196b9527c6c7ebc7336c2fcec
jaybuidl <jb@kleros.io>
2023-06-20 00:22:46 +0100

    feat: added a simple dispute template, moved IMetaEvidence to the v1 folder
    And removed intermediate interfaces

--- contracts/src/arbitration/IArbitratorV2.sol
+++ contracts/src/arbitration/IArbitratorV2.sol
@@ -23,13 +23,36 @@ interface IArbitratorV2 {
     /// @param _ruling The ruling which was given.
     event Ruling(IArbitrableV2 indexed _arbitrable, uint256 indexed _disputeID, uint256 _ruling);
 
-    /// @dev Create a dispute.
+    /// @dev To be emitted when an ERC20 token is added or removed as a method to pay fees.
+    /// @param _token The ERC20 token.
+    /// @param _accepted Whether the token is accepted or not.
+    event AcceptedFeeToken(address indexed _token, bool indexed _accepted);
+
+    /// @dev Create a dispute. 
     ///      Must be called by the arbitrable contract.
     ///      Must pay at least arbitrationCost(_extraData).
-    /// @param _choices Amount of choices the arbitrator can make in this dispute.
-    /// @param _extraData Can be used to give additional info on the dispute to be created.
+    /// @param _numberOfChoices The number of choices the arbitrator can choose from in this dispute.
+    /// @param _extraData Additional info about the dispute. We use it to pass the ID of the dispute's court (first 32 bytes), the minimum number of jurors required (next 32 bytes) and the ID of the specific dispute kit (last 32 bytes).
     /// @return disputeID The identifier of the dispute created.
-    function createDispute(uint256 _choices, bytes calldata _extraData) external payable returns (uint256 disputeID);
+    function createDispute(
+        uint256 _numberOfChoices,
+        bytes calldata _extraData
+    ) external payable returns (uint256 disputeID);
+
+    /// @dev Create a dispute with the fees paid in a supported ERC20 token.
+    ///      Must be called by the arbitrable contract.
+    ///      Must pay at least arbitrationCost(_extraData).
+    /// @param _numberOfChoices The number of choices the arbitrator can choose from in this dispute.
+    /// @param _extraData Additional info about the dispute. We use it to pass the ID of the dispute's court (first 32 bytes), the minimum number of jurors required (next 32 bytes) and the ID of the specific dispute kit (last 32 bytes).
+    /// @param _feeToken Address of the ERC20 token used to pay fees.
+    /// @param _feeAmount Amount of the ERC20 token used to pay fees.
+    /// @return disputeID The identifier of the dispute created.
+    function createDispute(
+        uint256 _numberOfChoices,
+        bytes calldata _extraData,
+        address _feeToken,
+        uint256 _feeAmount
+    ) external returns (uint256 disputeID);
 
     /// @dev Compute the cost of arbitration.
     ///      It is recommended not to increase it often, as it can be highly time and gas consuming for the arbitrated contracts to cope with fee augmentation.
