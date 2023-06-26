d2c93821b7d912aff366dd77c03daa46135df9fa
alcercu <333aleix333@gmail.com>
2023-06-26 17:26:12 +0200

    fix(web): useEvidenceGroup

--- web/src/hooks/queries/useEvidenceGroup.ts
+++ web/src/hooks/queries/useEvidenceGroup.ts
@@ -1,24 +1,26 @@
 import useSWRImmutable from "swr/immutable";
 import { getIArbitrableV2 } from "hooks/contracts/generated";
 import { usePublicClient } from "wagmi";
+import { isUndefined } from "utils/index";
 
-export const useEvidenceGroup = (disputeID?: string, arbitrableAddress?: string) => {
-  // const formattedAddress = arbitrableAddress
-  //   ? utils.getAddress(arbitrableAddress)
-  //   : undefined;
+export const useEvidenceGroup = (disputeID?: string, arbitrableAddress?: `0x${string}`) => {
   const publicClient = usePublicClient();
-
-  const arbitrable = getIArbitrableV2({
-    address: "0xc0fcc96BFd78e36550FCaB434A9EE1210B57225b",
-  });
-
   return useSWRImmutable(
-    () => (arbitrable ? `EvidenceGroup${disputeID}${arbitrableAddress}` : false),
+    () => (arbitrableAddress ? `EvidenceGroup${disputeID}${arbitrableAddress}` : false),
     async () => {
-      if (arbitrable) {
-        const disputeFilter = await arbitrable.createEventFilter.DisputeTemplate({
-          _templateId: BigInt(parseInt(disputeID!) + 1),
+      if (arbitrableAddress && !isUndefined(disputeID)) {
+        const arbitrable = getIArbitrableV2({
+          address: arbitrableAddress,
         });
+        const disputeFilter = await arbitrable.createEventFilter.DisputeTemplate(
+          {
+            _templateId: BigInt(disputeID),
+          },
+          {
+            fromBlock: 27808516n,
+            toBlock: "latest",
+          }
+        );
 
         const disputeEvents = await publicClient.getFilterLogs({
           filter: disputeFilter,
