a7fb5a8ee04d4ae3754e8031a2b04e88258687db
alcercu <333aleix333@gmail.com>
2023-07-12 10:10:37 +0200

    feat(web): add react-query provider

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -5,6 +5,7 @@ import { Routes, Route } from "react-router-dom";
 import "react-loading-skeleton/dist/skeleton.css";
 import "react-toastify/dist/ReactToastify.css";
 import Web3Provider from "context/Web3Provider";
+import QueryClientProvider from "context/QueryClientProvider";
 import StyledComponentsProvider from "context/StyledComponentsProvider";
 import Layout from "layout/index";
 import Home from "./pages/Home";
@@ -22,26 +23,28 @@ const fetcherBuilder =
 const App: React.FC = () => {
   return (
     <StyledComponentsProvider>
-      <SWRConfig
-        value={{
-          fetcher: fetcherBuilder(
-            process.env.REACT_APP_SUBGRAPH_ENDPOINT ??
-              "https://api.thegraph.com/subgraphs/name/kleros/kleros-v2-core-arbitrum-goerli"
-          ),
-        }}
-      >
-        <Web3Provider>
-          <Routes>
-            <Route path="/" element={<Layout />}>
-              <Route index element={<Home />} />
-              <Route path="cases/*" element={<Cases />} />
-              <Route path="courts/*" element={<Courts />} />
-              <Route path="dashboard" element={<Dashboard />} />
-              <Route path="*" element={<h1>Justice not found here ¯\_( ͡° ͜ʖ ͡°)_/¯</h1>} />
-            </Route>
-          </Routes>
-        </Web3Provider>
-      </SWRConfig>
+      <QueryClientProvider>
+        <SWRConfig
+          value={{
+            fetcher: fetcherBuilder(
+              process.env.REACT_APP_SUBGRAPH_ENDPOINT ??
+                "https://api.thegraph.com/subgraphs/name/kleros/kleros-v2-core-arbitrum-goerli"
+            ),
+          }}
+        >
+          <Web3Provider>
+            <Routes>
+              <Route path="/" element={<Layout />}>
+                <Route index element={<Home />} />
+                <Route path="cases/*" element={<Cases />} />
+                <Route path="courts/*" element={<Courts />} />
+                <Route path="dashboard" element={<Dashboard />} />
+                <Route path="*" element={<h1>Justice not found here ¯\_( ͡° ͜ʖ ͡°)_/¯</h1>} />
+              </Route>
+            </Routes>
+          </Web3Provider>
+        </SWRConfig>
+      </QueryClientProvider>
     </StyledComponentsProvider>
   );
 };
