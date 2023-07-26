a6b0dee232d4ccde22c0cd0f3b05f072bcd24112
jaybuidl <jb@kleros.io>
2023-07-26 20:52:14 +0100

    feat: generate the wagmi config dynamically by reading the deployment artifacts from all the chains

--- contracts/wagmi.config.ts
+++ contracts/wagmi.config.ts
@@ -1,8 +1,9 @@
+import { readdir, readFile } from "fs/promises";
+import { parse, join } from "path";
 import { Config, ContractConfig, defineConfig } from "@wagmi/cli";
-import { arbitrumGoerli, gnosisChiado } from "wagmi/chains";
-import HomeGatewayToGnosis from "@kleros/kleros-v2-contracts/deployments/arbitrumGoerli/HomeGatewayToGnosis.json" assert { type: "json" };
-import IHomeGateway from "@kleros/kleros-v2-contracts/artifacts/src/gateway/interfaces/IHomeGateway.sol/IHomeGateway.json" assert { type: "json" };
 import { Abi } from "viem";
+import { Chain } from "@wagmi/chains";
+import IHomeGateway from "@kleros/kleros-v2-contracts/artifacts/src/gateway/interfaces/IHomeGateway.sol/IHomeGateway.json" assert { type: "json" };
 
 type ArtifactPartial = {
   abi: Abi;
@@ -12,17 +13,84 @@ const getAbi = (artifact: any) => {
   return (artifact as ArtifactPartial).abi;
 };
 
+const readArtifacts = async (viemChainName: string, hardhatChainName?: string) => {
+  const chains = await import("wagmi/chains");
+  const chain = chains[viemChainName] as Chain;
+  if (!chain) {
+    throw new Error(`Viem chain ${viemChainName} not found`);
+  }
+
+  const directoryPath = `./deployments/${hardhatChainName ?? viemChainName}`;
+  const files = await readdir(directoryPath);
+
+  const results: ContractConfig[] = [];
+  for (const file of files) {
+    const { name, ext } = parse(file);
+    if (ext === ".json") {
+      const filePath = join(directoryPath, file);
+      const fileContent = await readFile(filePath, "utf-8");
+      const jsonContent = JSON.parse(fileContent);
+      results.push({
+        name: name,
+        address: {
+          [chain.id]: jsonContent.address as `0x{string}`,
+        },
+        abi: jsonContent.abi,
+      });
+    }
+  }
+  return results;
+};
+
+// Group contracts by name and merge the address dictionary
+function merge(arr1: ContractConfig[], arr2: ContractConfig[]) {
+  const mergedArr: ContractConfig[] = [...arr1];
+  for (const contract of arr2) {
+    const index = mergedArr.findIndex((c) => c.name === contract.name);
+    if (index === -1) {
+      mergedArr.push(contract);
+    } else {
+      mergedArr[index] = {
+        ...mergedArr[index],
+        address: {
+          ...(mergedArr[index].address as Record<number, `0x${string}`>),
+          ...(contract.address as Record<number, `0x${string}`>),
+        },
+      };
+    }
+  }
+  return mergedArr;
+}
+
 const getConfig = async (): Promise<Config> => {
+  const arbitrumGoerliContracts = await readArtifacts("arbitrumGoerli");
+  arbitrumGoerliContracts.forEach((c) => console.log("✔ Found arbitrumGoerli artifact: %s", c.name));
+  let contracts = arbitrumGoerliContracts;
+
+  const chiadoContracts = await readArtifacts("gnosisChiado", "chiado"); // renaming the Hardhat network improves this but breaks many other scripts
+  chiadoContracts.forEach((c) => console.log("✔ Found chiado artifact: %s", c.name));
+  contracts = merge(contracts, chiadoContracts);
+
+  const goerliContracts = await readArtifacts("goerli");
+  goerliContracts.forEach((c) => console.log("✔ Found goerli artifact: %s", c.name));
+  contracts = merge(contracts, goerliContracts);
+
+  const arbitrumContracts = await readArtifacts("arbitrum");
+  arbitrumContracts.forEach((c) => console.log("✔ Found arbitrum artifact: %s", c.name));
+  contracts = merge(contracts, arbitrumContracts);
+
+  const gnosisContracts = await readArtifacts("gnosis", "gnosischain");
+  gnosisContracts.forEach((c) => console.log("✔ Found gnosis artifact: %s", c.name));
+  contracts = merge(contracts, gnosisContracts);
+
+  const mainnetContracts = await readArtifacts("mainnet");
+  mainnetContracts.forEach((c) => console.log("✔ Found mainnet artifact: %s", c.name));
+  contracts = merge(contracts, mainnetContracts);
+
   return {
     out: "viem/generated.ts",
     contracts: [
-      {
-        name: "HomeGatewayToGnosis",
-        address: {
-          [arbitrumGoerli.id]: HomeGatewayToGnosis.address as `0x{string}`,
-        },
-        abi: getAbi(HomeGatewayToGnosis),
-      },
+      ...contracts,
       {
         name: "IHomeGateway",
         abi: getAbi(IHomeGateway),
