819bdee71d4e2f164f012084bf6a2aad2504cc4a
Zishan Mohd <zishan2539@gmail.com>
2023-07-28 22:43:58 +0530

    feat: added view filter in latest cases component

---
+++ web/src/components/CasesDisplay/ViewFilter.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import styled, { css } from "styled-components";
+import CardView from "svgs/icons/card-view.svg";
+import ListViewIcon from "svgs/icons/list-view.svg";
+
+export type ViewFilterType = "card-view" | "list-view";
+
+interface IViewFilter {
+  view: ViewFilterType;
+  setView: React.Dispatch<React.SetStateAction<ViewFilterType>>;
+}
+
+interface IStyledIcon {
+  isActive: boolean;
+}
+
+const fillWithPrimaryBlue = css`
+  fill: ${({ theme }) => theme.primaryBlue};
+`;
+
+const fillWithStroke = css`
+  fill: ${({ theme }) => theme.stroke};
+`;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: end;
+  gap: 12px;
+  width: fit-content;
+  margin-left: auto;
+`;
+
+const StyledCardViewIcon = styled(CardView || ListViewIcon)<IStyledIcon>`
+  ${(props) => (props.isActive ? fillWithPrimaryBlue : fillWithStroke)}
+  cursor:pointer;
+`;
+
+const StyledListViewIcon = styled(ListViewIcon)<IStyledIcon>`
+  ${(props) => (props.isActive ? fillWithPrimaryBlue : fillWithStroke)}
+  display:block;
+  cursor: pointer;
+  @media (max-width: 768px) {
+    display: none;
+  }
+`;
+
+const ViewFilter: React.FC<IViewFilter> = ({ view, setView }) => {
+  const handleViewChange = (view: ViewFilterType) => {
+    setView(view);
+  };
+
+  return (
+    <Container>
+      <StyledCardViewIcon isActive={view === "card-view"} onClick={() => handleViewChange("card-view")} />
+      <StyledListViewIcon isActive={view === "list-view"} onClick={() => handleViewChange("list-view")} />
+    </Container>
+  );
+};
+
+export default ViewFilter;
--- web/src/pages/Home/LatestCases.tsx
+++ web/src/pages/Home/LatestCases.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { useCasesQuery } from "queries/useCasesQuery";
 import DisputeCard from "components/DisputeCard";
+import ViewFilter, { ViewFilterType } from "~src/components/CasesDisplay/ViewFilter";
 
 const Container = styled.div`
   margin-top: 64px;
@@ -15,9 +16,11 @@ const Container = styled.div`
 
 const LatestCases: React.FC = () => {
   const { data } = useCasesQuery(0);
+  const [view, setView] = useState<ViewFilterType>("card-view");
   return (
     <Container>
       <h1>Latest Cases</h1>
+      <ViewFilter view={view} setView={setView} />
       <div className="disputes-container">
         {data?.disputes.map((dispute, i) => (
           <DisputeCard key={i} {...dispute} />
