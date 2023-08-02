8d3d1923ccc803e3679af35759560ade594f537e
alcercu <333aleix333@gmail.com>
2023-08-02 20:25:57 +0200

    refactor(web): use subgraph instead of calling contract for overridden disputes

--- web/src/components/Verdict/DisputeTimeline.tsx
+++ web/src/components/Verdict/DisputeTimeline.tsx
@@ -3,7 +3,6 @@ import { useParams } from "react-router-dom";
 import styled, { useTheme } from "styled-components";
 import { _TimelineItem1, CustomTimeline } from "@kleros/ui-components-library";
 import { Periods } from "consts/periods";
-import { useKlerosCoreCurrentRuling } from "hooks/contracts/generated";
 import { ClassicRound } from "src/graphql/graphql";
 import { DisputeDetailsQuery, useDisputeDetailsQuery } from "queries/useDisputeDetailsQuery";
 import { useDisputeTemplate } from "queries/useDisputeTemplate";
@@ -65,43 +64,51 @@ const useItems = (disputeDetails?: DisputeDetailsQuery, arbitrable?: `0x${string
   const { id } = useParams();
   const { data: votingHistory } = useVotingHistory(id);
   const { data: disputeTemplate } = useDisputeTemplate(id, arbitrable);
-  const { data: currentRulingArray } = useKlerosCoreCurrentRuling({ args: [BigInt(id ?? 0)], watch: true });
-  const rulingOverride = currentRulingArray?.[2];
   const localRounds: ClassicRound[] = votingHistory?.dispute?.disputeKitDispute?.localRounds;
   const theme = useTheme();
 
   return useMemo<TimelineItems | undefined>(() => {
     const dispute = disputeDetails?.dispute;
     if (dispute) {
+      const rulingOverride = dispute.overridden;
+      const parsedDisputeFinalRuling = parseInt(dispute.currentRuling);
       const currentPeriodIndex = Periods[dispute.period];
       const lastPeriodChange = dispute.lastPeriodChange;
       const courtTimePeriods = dispute.court.timesPerPeriod;
       return localRounds?.reduce<TimelineItems>(
         (acc, { winningChoice }, index) => {
-          const parsedWinningChoice = parseInt(winningChoice);
+          const parsedRoundChoice = parseInt(winningChoice);
           const eventDate = getCaseEventTimes(lastPeriodChange, currentPeriodIndex, courtTimePeriods, false);
-          const icon =
-            disputeDetails?.dispute?.ruled && !rulingOverride && index === localRounds.length - 1 ? ClosedCaseIcon : "";
+          const icon = dispute.ruled && !rulingOverride && index === localRounds.length - 1 ? ClosedCaseIcon : "";
 
           acc.push({
             title: `Jury Decision - Round ${index + 1}`,
             party:
-              parsedWinningChoice !== 0
-                ? disputeTemplate?.answers?.[parsedWinningChoice - 1].title
-                : "Refuse to Arbitrate",
+              parsedRoundChoice !== 0 ? disputeTemplate?.answers?.[parsedRoundChoice - 1].title : "Refuse to Arbitrate",
             subtitle: eventDate,
             rightSided: true,
             variant: theme.secondaryPurple,
             Icon: icon !== "" ? icon : undefined,
           });
 
-          if (index < localRounds.length - 1 || rulingOverride) {
+          if (index < localRounds.length - 1) {
             acc.push({
               title: "Appealed",
               party: "",
               subtitle: eventDate,
               rightSided: true,
               Icon: AppealedCaseIcon,
+            });
+          } else if (rulingOverride && parsedDisputeFinalRuling !== parsedRoundChoice) {
+            acc.push({
+              title: "Won by Appeal",
+              party:
+                parsedDisputeFinalRuling !== 0
+                  ? disputeTemplate?.answers?.[parsedDisputeFinalRuling - 1].title
+                  : "Refuse to Arbitrate",
+              subtitle: eventDate,
+              rightSided: true,
+              Icon: ClosedCaseIcon,
             });
           }
 
