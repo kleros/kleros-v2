d9b6d9d6bd4bc7df0f051c6db09ac5292665499c
zmalatrax <simon.malatrait@grenoble-inp.org>
2023-08-09 02:08:51 +0200

    test(contracts): mock implementation to test proxy

---
+++ contracts/src/proxy/mock/MockImplementations.sol
@@ -0,0 +1,62 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.18;
+
+import "../UUPSProxiable.sol";
+
+contract NonUpgradeableMock {
+    uint256 public _counter;
+
+    function counter() external view returns (uint256) {
+        return _counter;
+    }
+
+    function increment() external {
+        _counter++;
+    }
+
+    function version() external pure virtual returns (string memory) {
+        return "NonUpgradeableMock 0.0.0";
+    }
+}
+
+contract UUPSUpgradeableMock is UUPSProxiable, NonUpgradeableMock {
+    bool private initialized;
+    address public governor;
+
+    uint256[50] __gap;
+
+    constructor() {
+        initialized = true;
+    }
+
+    function initialize(address _governor) external {
+        require(!initialized, "Contract instance has already been initialized");
+        governor = _governor;
+        initialized = true;
+    }
+
+    function _authorizeUpgrade(address) internal view override {
+        require(governor == msg.sender, "No privilege to upgrade");
+    }
+
+    function version() external pure virtual override returns (string memory) {
+        return "UUPSUpgradeableMock 1.0.0";
+    }
+}
+
+contract UUPSUpgradeableMockV2 is UUPSUpgradeableMock {
+    function version() external pure override returns (string memory) {
+        return "UUPSUpgradeableMock 2.0.0";
+    }
+}
+
+contract UUPSUnsupportedProxiableUUID is UUPSUpgradeableMock {
+    function proxiableUUID() external pure override returns (bytes32) {
+        return keccak256("invalid UUID");
+    }
+
+    function version() external pure override returns (string memory) {
+        return "UUPSUnsupportedProxiableUUID 1.0.0";
+    }
+}
