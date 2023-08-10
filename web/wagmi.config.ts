6ca3fbcd18eb990d878f69cdef52a723aba4bb69
jaybuidl <jb@kleros.io>
2023-08-10 03:58:27 +0100

    feat: support for multiple deployments for wagmi

--- web/wagmi.config.ts
+++ web/wagmi.config.ts
@@ -1,60 +1,89 @@
-import { defineConfig } from "@wagmi/cli";
-import { react, actions } from "@wagmi/cli/plugins";
-import { arbitrumGoerli } from "wagmi/chains";
-import KlerosCore from "@kleros/kleros-v2-contracts/deployments/arbitrumGoerli/KlerosCore.json" assert { type: "json" };
-import PNK from "@kleros/kleros-v2-contracts/deployments/arbitrumGoerli/PNK.json" assert { type: "json" };
-import DisputeKitClassic from "@kleros/kleros-v2-contracts/deployments/arbitrumGoerli/DisputeKitClassic.json" assert { type: "json" };
-import PolicyRegistry from "@kleros/kleros-v2-contracts/deployments/arbitrumGoerli/PolicyRegistry.json" assert { type: "json" };
-import IArbitrableV2 from "@kleros/kleros-v2-contracts/artifacts/src/arbitration/interfaces/IArbitrableV2.sol/IArbitrableV2.json" assert { type: "json" };
+import { Config, ContractConfig, defineConfig } from "@wagmi/cli";
+import { readdir, readFile } from "fs/promises";
+import { parse, join } from "path";
+import { Abi } from "viem";
+import { Chain } from "@wagmi/chains";
+import dotenv from "dotenv";
 import IHomeGateway from "@kleros/kleros-v2-contracts/artifacts/src/gateway/interfaces/IHomeGateway.sol/IHomeGateway.json" assert { type: "json" };
-import PNKFaucet from "@kleros/kleros-v2-contracts/deployments/arbitrumGoerli/PNKFaucet.json" assert { type: "json" };
+import IArbitrableV2 from "@kleros/kleros-v2-contracts/artifacts/src/arbitration/interfaces/IArbitrableV2.sol/IArbitrableV2.json" assert { type: "json" };
 
-export default defineConfig({
-  out: "src/hooks/contracts/generated.ts",
-  contracts: [
-    {
-      name: "KlerosCore",
-      address: {
-        [arbitrumGoerli.id]: KlerosCore.address as `0x{string}`,
+dotenv.config();
+
+type ArtifactPartial = {
+  abi: Abi;
+};
+
+const getAbi = (artifact: any) => {
+  return (artifact as ArtifactPartial).abi;
+};
+
+const readArtifacts = async (viemChainName: string, hardhatChainName?: string) => {
+  const chains = await import("wagmi/chains");
+  const chain = chains[viemChainName] as Chain;
+  if (!chain) {
+    throw new Error(`Viem chain ${viemChainName} not found`);
+  }
+
+  const directoryPath = `../contracts/deployments/${hardhatChainName ?? viemChainName}`;
+  const files = await readdir(directoryPath);
+
+  const results: ContractConfig[] = [];
+  for (const file of files) {
+    const { name, ext } = parse(file);
+    if (ext === ".json") {
+      const filePath = join(directoryPath, file);
+      const fileContent = await readFile(filePath, "utf-8");
+      const jsonContent = JSON.parse(fileContent);
+      results.push({
+        name: name,
+        address: {
+          [chain.id]: jsonContent.address as `0x{string}`,
+        },
+        abi: jsonContent.abi,
+      });
+    }
+  }
+  return results;
+};
+
+const getConfig = async (): Promise<Config> => {
+  const deployment = process.env["REACT_APP_DEPLOYMENT"] ?? "testnet";
+
+  let viemNetwork;
+  let hardhatNetwork;
+  switch (deployment) {
+    case "devnet":
+      viemNetwork = "arbitrumGoerli";
+      hardhatNetwork = "arbitrumGoerliDevnet";
+      break;
+    case "testnet":
+      viemNetwork = "arbitrumGoerli";
+      hardhatNetwork = "arbitrumGoerli";
+      break;
+    case "mainnet":
+      viemNetwork = "arbitrum";
+      hardhatNetwork = "arbitrum";
+      break;
+    default:
+      throw new Error(`Unknown deployment ${deployment}`);
+  }
+
+  const deploymentContracts = await readArtifacts(viemNetwork, hardhatNetwork);
+
+  return {
+    out: "src/hooks/contracts/generated.ts",
+    contracts: [
+      ...deploymentContracts,
+      {
+        name: "IHomeGateway",
+        abi: getAbi(IHomeGateway),
       },
-      abi: KlerosCore.abi,
-    },
-    {
-      name: "DisputeKitClassic",
-      address: {
-        [arbitrumGoerli.id]: DisputeKitClassic.address as `0x{string}`,
+      {
+        name: "IArbitrableV2",
+        abi: getAbi(IArbitrableV2),
       },
-      abi: DisputeKitClassic.abi,
-    },
-    {
-      name: "PNK",
-      address: {
-        [arbitrumGoerli.id]: PNK.address as `0x{string}`,
-      },
-      abi: PNK.abi,
-    },
-    {
-      name: "PolicyRegistry",
-      address: {
-        [arbitrumGoerli.id]: PolicyRegistry.address as `0x{string}`,
-      },
-      abi: PolicyRegistry.abi,
-    },
-    {
-      name: "IArbitrableV2",
-      abi: IArbitrableV2.abi,
-    },
-    {
-      name: "IHomeGateway",
-      abi: IHomeGateway.abi,
-    },
-    {
-      name: "PNKFaucet",
-      address: {
-        [arbitrumGoerli.id]: PNKFaucet.address as `0x{string}`,
-      },
-      abi: PNKFaucet.abi,
-    },
-  ],
-  plugins: [react(), actions()],
-});
+    ],
+  };
+};
+
+export default defineConfig(getConfig);
