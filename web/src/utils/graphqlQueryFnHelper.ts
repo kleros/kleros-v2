b852e95be18acfd9f24e8a054ba786d61bdfe826
alcercu <333aleix333@gmail.com>
2023-08-10 12:52:55 +0200

    feat: add deployment dependant subgraphs

--- web/src/utils/graphqlQueryFnHelper.ts
+++ web/src/utils/graphqlQueryFnHelper.ts
@@ -1,8 +1,12 @@
 import request from "graphql-request";
 import { TypedDocumentNode } from "@graphql-typed-document-node/core";
 
+const DEPLOYMENT = process.env["REACT_APP_DEPLOYMENT"]?.toUpperCase() ?? "TESTNET";
+
 const CHAINID_TO_DISPUTETEMPLATE_SUBGRAPH = {
-  421613: "https://api.thegraph.com/subgraphs/name/alcercu/disputetemplateregistryarbgrli",
+  421613:
+    process.env[`REACT_APP_DISPUTE_TEMPLATE_ARBGOERLI_SUBGRAPH_${DEPLOYMENT}`] ??
+    "https://api.thegraph.com/subgraphs/name/alcercu/disputetemplateregistryarbgrli",
 };
 
 export const graphqlQueryFnHelper = async (
@@ -12,7 +16,8 @@ export const graphqlQueryFnHelper = async (
   chainId = 421613
 ) => {
   const coreUrl =
-    process.env.REACT_APP_SUBGRAPH_ENDPOINT ?? "https://api.thegraph.com/subgraphs/name/alcercu/kleroscoretest";
+    process.env[`REACT_APP_KLEROS_CORE_SUBGRAPH_${DEPLOYMENT}`] ??
+    "https://api.thegraph.com/subgraphs/name/alcercu/disputetemplateregistryarbgrli";
   const url = isDisputeTemplate ? CHAINID_TO_DISPUTETEMPLATE_SUBGRAPH[chainId] : coreUrl;
   return request(url, query, parametersObject);
 };
