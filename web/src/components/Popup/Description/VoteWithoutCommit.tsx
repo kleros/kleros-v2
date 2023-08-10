cc27aae1fd167870f22bebd77f44e7290ce72a83
marino <102478601+kemuru@users.noreply.github.com>
2023-08-10 13:39:34 +0200

    fix(web): css problem on Voting popup

--- web/src/components/Popup/Description/VoteWithoutCommit.tsx
+++ web/src/components/Popup/Description/VoteWithoutCommit.tsx
@@ -7,10 +7,10 @@ interface IVoteWithoutCommit {
 
 const VoteWithoutCommit: React.FC<IVoteWithoutCommit> = ({ date }) => {
   return (
-    <>
+    <div>
       The decision date is <VoteDescriptionEmphasizedDate>{date}</VoteDescriptionEmphasizedDate> with the possibility
       for appeals. After that time you will be informed about the jury decision.
-    </>
+    </div>
   );
 };
 export default VoteWithoutCommit;
