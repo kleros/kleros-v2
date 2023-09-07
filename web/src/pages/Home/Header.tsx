6fd16e89b6420b970f8921da82e88ebe078f9f5f
marino <102478601+kemuru@users.noreply.github.com>
2023-09-07 11:19:31 +0200

    refactor(web): use different hook for window size

--- web/src/pages/Home/Header.tsx
+++ web/src/pages/Home/Header.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 import { useTheme } from "styled-components";
+import { useWindowSize } from "react-use";
 import { BREAKPOINT_SMALL_SCREEN } from "styles/smallScreenStyle";
 import HeaderLightMobile from "tsx:svgs/header/header-lightmode-mobile.svg";
 import HeaderDarkMobile from "tsx:svgs/header/header-darkmode-mobile.svg";
 import HeaderLightDesktop from "tsx:svgs/header/header-lightmode-desktop.svg";
 import HeaderDarkDesktop from "tsx:svgs/header/header-darkmode-desktop.svg";
-import { useWindowWidth } from "hooks/useWindowWidth";
 
 const Header = () => {
-  const windowWidth = useWindowWidth();
+  const { width } = useWindowSize();
   const theme = useTheme();
   const themeIsLight = theme.name === "light";
-  const screenIsBig = windowWidth > BREAKPOINT_SMALL_SCREEN;
+  const screenIsBig = width > BREAKPOINT_SMALL_SCREEN;
   return <div>{screenIsBig ? <HeaderDesktop {...{ themeIsLight }} /> : <HeaderMobile {...{ themeIsLight }} />}</div>;
 };
 
