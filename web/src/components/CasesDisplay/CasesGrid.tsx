61c67b687bc63ca6ae4bdcb409cd64edc187558a
alcercu <333aleix333@gmail.com>
2023-10-04 17:53:31 +0200

    chore(web): remove unused context

--- web/src/components/CasesDisplay/CasesGrid.tsx
+++ web/src/components/CasesDisplay/CasesGrid.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import styled from "styled-components";
+import { useParams } from "react-router-dom";
 import Skeleton from "react-loading-skeleton";
 import { StandardPagination } from "@kleros/ui-components-library";
 import { isUndefined } from "utils/index";
+import { decodeURIFilter } from "utils/uri";
 import { DisputeDetailsFragment } from "queries/useCasesQuery";
-import { useFiltersContext } from "context/FilterProvider";
 import DisputeCard from "components/DisputeCard";
 
 const Container = styled.div`
@@ -43,7 +44,9 @@ const CasesGrid: React.FC<ICasesGrid> = ({
   currentPage,
   setCurrentPage,
 }) => {
-  const { debouncedSearch } = useFiltersContext();
+  const { filter } = useParams();
+  const decodedFilter = decodeURIFilter(filter ?? "all");
+  const { id: searchValue } = decodedFilter;
 
   return (
     <>
@@ -57,13 +60,13 @@ const CasesGrid: React.FC<ICasesGrid> = ({
         </Container>
       )}
 
-      {debouncedSearch === "" && (
+      {isUndefined(searchValue) ? (
         <StyledPagination
           currentPage={currentPage}
           numPages={Math.ceil(totalPages ?? 0)}
           callback={(page: number) => setCurrentPage(page)}
         />
-      )}
+      ) : null}
     </>
   );
 };
