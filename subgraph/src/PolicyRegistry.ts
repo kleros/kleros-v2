9f3a289cd09bfb964ef8b135444b4db25ca859ee
shotaronowhere <shawtarohgn@gmail.com>
2023-10-17 19:21:03 -0700

    chore: subgraph refactor

--- subgraph/src/PolicyRegistry.ts
+++ subgraph/src/PolicyRegistry.ts
@@ -1,6 +1,6 @@
 import { PolicyUpdate } from "../generated/PolicyRegistry/PolicyRegistry";
 import { Court } from "../generated/schema";
-import { ipfs, log, json, Bytes, TypedMap, JSONValue } from "@graphprotocol/graph-ts";
+import { ipfs, json, Bytes, TypedMap, JSONValue } from "@graphprotocol/graph-ts";
 
 export function handlePolicyUpdate(event: PolicyUpdate): void {
   const courtID = event.params._courtID.toString();
@@ -10,13 +10,7 @@ export function handlePolicyUpdate(event: PolicyUpdate): void {
     court.name = event.params._courtName;
     court.save();
 
-    let jsonStr = ipfs.cat(event.params._policy);
-    if (!jsonStr) return;
-
-    const jsonObjValueAndSuccess = json.try_fromBytes(jsonStr as Bytes);
-    if (!jsonObjValueAndSuccess.isOk) return;
-
-    const jsonObj = jsonObjValueAndSuccess.value.toObject();
+    const jsonObj = getJsonObj(event.params._policy);
     if (!jsonObj) return;
 
     court.name = tryGetValue(jsonObj, "name");
@@ -29,4 +23,17 @@ export function handlePolicyUpdate(event: PolicyUpdate): void {
 function tryGetValue(jsonObj: TypedMap<string, JSONValue>, key: string): string | null {
   const value = jsonObj.get(key);
   return value ? value.toString() : null;
+}
+
+function getJsonObj(CID: string): TypedMap<string, JSONValue> | null {
+  const jsonStr = ipfs.cat(CID);
+  if (!jsonStr) return null;
+
+  const jsonObjValueAndSuccess = json.try_fromBytes(jsonStr as Bytes);
+  if (!jsonObjValueAndSuccess.isOk) return null;
+
+  const jsonObj = jsonObjValueAndSuccess.value.toObject();
+  if (!jsonObj) return null;
+
+  return jsonObj;
 }
