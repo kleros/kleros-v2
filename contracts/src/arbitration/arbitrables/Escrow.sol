56bb341e645a9db9d0c5dafc5fbb7be1e2a24e4e
jaybuidl <jb@kleros.io>
2023-12-07 11:48:31 +0000

    fix: style

--- contracts/src/arbitration/arbitrables/Escrow.sol
+++ contracts/src/arbitration/arbitrables/Escrow.sol
@@ -1,28 +1,22 @@
 // SPDX-License-Identifier: MIT
 
-/**
- *  @authors: [@unknownunknown1, @fnanni-0, @shalzz]
- *  @reviewers: []
- *  @auditors: []
- *  @bounties: []
- */
+/// @authors: [@unknownunknown1, @fnanni-0, @shalzz]
+/// @reviewers: []
+/// @auditors: []
+/// @bounties: []
 
 pragma solidity 0.8.18;
 
 import {IArbitrableV2, IArbitratorV2} from "../interfaces/IArbitrableV2.sol";
 import "../interfaces/IDisputeTemplateRegistry.sol";
 
-/**
- *  @title Escrow
- *  @dev MultipleArbitrableTransaction contract that is compatible with V2.
- *  https://github.com/kleros/kleros-interaction/blob/master/contracts/standard/arbitration/MultipleArbitrableTransaction.sol
- */
+/// @title Escrow
+/// @dev MultipleArbitrableTransaction contract that is compatible with V2.
+///      Adapted from https://github.com/kleros/kleros-interaction/blob/master/contracts/standard/arbitration/MultipleArbitrableTransaction.sol
 contract Escrow is IArbitrableV2 {
-    // **************************** //
-    // *    Contract variables    * //
-    // **************************** //
-
-    uint256 public constant AMOUNT_OF_CHOICES = 2;
+    // ************************************* //
+    // *         Enums / Structs           * //
+    // ************************************* //
 
     enum Party {
         None,
@@ -59,40 +53,39 @@ contract Escrow is IArbitrableV2 {
         Status status;
     }
 
+    // ************************************* //
+    // *             Storage               * //
+    // ************************************* //
+
+    uint256 public constant AMOUNT_OF_CHOICES = 2;
     address public immutable governor;
-    IArbitratorV2 public arbitrator; // Address of the arbitrator contract. TRUSTED.
+    IArbitratorV2 public arbitrator; // Address of the arbitrator contract.
     bytes public arbitratorExtraData; // Extra data to set up the arbitration.
     IDisputeTemplateRegistry public templateRegistry; // The dispute template registry.
-
     uint256 public immutable feeTimeout; // Time in seconds a party can take to pay arbitration fees before being considered unresponsive and lose the dispute.
-    /// @dev List of all created transactions.
-    Transaction[] public transactions;
-
+    Transaction[] public transactions; // List of all created transactions.
     mapping(uint256 => uint256) public disputeIDtoTransactionID; // Naps dispute ID to tx ID.
 
-    // **************************** //
-    // *          Events          * //
-    // **************************** //
+    // ************************************* //
+    // *              Events               * //
+    // ************************************* //
 
-    /** @dev To be emitted when a party pays or reimburses the other.
-     *  @param _transactionID The index of the transaction.
-     *  @param _amount The amount paid.
-     *  @param _party The party that paid.
-     */
+    /// @dev To be emitted when a party pays or reimburses the other.
+    /// @param _transactionID The index of the transaction.
+    /// @param _amount The amount paid.
+    /// @param _party The party that paid.
     event Payment(uint256 indexed _transactionID, uint256 _amount, address _party);
 
-    /** @dev Indicate that a party has to pay a fee or would otherwise be considered as losing.
-     *  @param _transactionID The index of the transaction.
-     *  @param _party The party who has to pay.
-     */
+    /// @dev Indicate that a party has to pay a fee or would otherwise be considered as losing.
+    /// @param _transactionID The index of the transaction.
+    /// @param _party The party who has to pay.
     event HasToPayFee(uint256 indexed _transactionID, Party _party);
 
-    /** @dev Emitted when a transaction is created.
-     *  @param _transactionID The index of the transaction.
-     *  @param _sender The address of the sender.
-     *  @param _receiver The address of the receiver.
-     *  @param _amount The initial amount in the transaction.
-     */
+    /// @dev Emitted when a transaction is created.
+    /// @param _transactionID The index of the transaction.
+    /// @param _sender The address of the sender.
+    /// @param _receiver The address of the receiver.
+    /// @param _amount The initial amount in the transaction.
     event TransactionCreated(
         uint256 indexed _transactionID,
         address indexed _sender,
@@ -100,24 +93,30 @@ contract Escrow is IArbitrableV2 {
         uint256 _amount
     );
 
-    /** @dev To be emitted when a transaction is resolved, either by its
-     *  execution, a timeout or because a ruling was enforced.
-     *  @param _transactionID The ID of the respective transaction.
-     *  @param _resolution Short description of what caused the transaction to be solved.
-     */
+    /// @dev To be emitted when a transaction is resolved, either by its
+    ///      execution, a timeout or because a ruling was enforced.
+    /// @param _transactionID The ID of the respective transaction.
+    /// @param _resolution Short description of what caused the transaction to be solved.
     event TransactionResolved(uint256 indexed _transactionID, Resolution indexed _resolution);
+
+    // ************************************* //
+    // *        Function Modifiers         * //
+    // ************************************* //
 
     modifier onlyByGovernor() {
         require(address(this) == msg.sender, "Only the governor allowed.");
         _;
     }
 
-    /** @dev Constructor.
-     *  @param _arbitrator The arbitrator of the contract.
-     *  @param _arbitratorExtraData Extra data for the arbitrator.
-     *  @param _templateRegistry The dispute template registry.
-     *  @param _feeTimeout Arbitration fee timeout for the parties.
-     */
+    // ************************************* //
+    // *            Constructor            * //
+    // ************************************* //
+
+    /// @dev Constructor.
+    /// @param _arbitrator The arbitrator of the contract.
+    /// @param _arbitratorExtraData Extra data for the arbitrator.
+    /// @param _templateRegistry The dispute template registry.
+    /// @param _feeTimeout Arbitration fee timeout for the parties.
     constructor(
         IArbitratorV2 _arbitrator,
         bytes memory _arbitratorExtraData,
@@ -147,13 +146,16 @@ contract Escrow is IArbitrableV2 {
         templateRegistry = _templateRegistry;
     }
 
-    /** @dev Create a transaction.
-     *  @param _timeoutPayment Time after which a party can automatically execute the arbitrable transaction.
-     *  @param _receiver The recipient of the transaction.
-     *  @param _templateData The dispute template data.
-     *  @param _templateDataMappings The dispute template data mappings.
-     *  @return transactionID The index of the transaction.
-     */
+    // ************************************* //
+    // *         State Modifiers           * //
+    // ************************************* //
+
+    /// @dev Create a transaction.
+    /// @param _timeoutPayment Time after which a party can automatically execute the arbitrable transaction.
+    /// @param _receiver The recipient of the transaction.
+    /// @param _templateData The dispute template data.
+    /// @param _templateDataMappings The dispute template data mappings.
+    /// @return transactionID The index of the transaction.
     function createTransaction(
         uint256 _timeoutPayment,
         address payable _receiver,
@@ -173,10 +175,9 @@ contract Escrow is IArbitrableV2 {
         emit TransactionCreated(transactionID, msg.sender, _receiver, msg.value);
     }
 
-    /** @dev Pay receiver. To be called if the good or service is provided.
-     *  @param _transactionID The index of the transaction.
-     *  @param _amount Amount to pay in wei.
-     */
+    /// @dev Pay receiver. To be called if the good or service is provided.
+    /// @param _transactionID The index of the transaction.
+    /// @param _amount Amount to pay in wei.
     function pay(uint256 _transactionID, uint256 _amount) external {
         Transaction storage transaction = transactions[_transactionID];
         require(transaction.sender == msg.sender, "The caller must be the sender.");
@@ -189,10 +190,9 @@ contract Escrow is IArbitrableV2 {
         emit Payment(_transactionID, _amount, msg.sender);
     }
 
-    /** @dev Reimburse sender. To be called if the good or service can't be fully provided.
-     *  @param _transactionID The index of the transaction.
-     *  @param _amountReimbursed Amount to reimburse in wei.
-     */
+    /// @dev Reimburse sender. To be called if the good or service can't be fully provided.
+    /// @param _transactionID The index of the transaction.
+    /// @param _amountReimbursed Amount to reimburse in wei.
     function reimburse(uint256 _transactionID, uint256 _amountReimbursed) external {
         Transaction storage transaction = transactions[_transactionID];
         require(transaction.receiver == msg.sender, "The caller must be the receiver.");
@@ -205,9 +205,8 @@ contract Escrow is IArbitrableV2 {
         emit Payment(_transactionID, _amountReimbursed, msg.sender);
     }
 
-    /** @dev Transfer the transaction's amount to the receiver if the timeout has passed.
-     *  @param _transactionID The index of the transaction.
-     */
+    /// @dev Transfer the transaction's amount to the receiver if the timeout has passed.
+    /// @param _transactionID The index of the transaction.
     function executeTransaction(uint256 _transactionID) external {
         Transaction storage transaction = transactions[_transactionID];
         require(block.timestamp >= transaction.deadline, "Deadline not passed.");
@@ -221,12 +220,11 @@ contract Escrow is IArbitrableV2 {
         emit TransactionResolved(_transactionID, Resolution.TransactionExecuted);
     }
 
-    /** @dev Pay the arbitration fee to raise a dispute. To be called by the sender. UNTRUSTED.
-     *  Note that the arbitrator can have createDispute throw, which will make
-     *  this function throw and therefore lead to a party being timed-out.
-     *  This is not a vulnerability as the arbitrator can rule in favor of one party anyway.
-     *  @param _transactionID The index of the transaction.
-     */
+    /// @dev Pay the arbitration fee to raise a dispute. To be called by the sender.
+    /// Note that the arbitrator can have createDispute throw, which will make
+    ///      this function throw and therefore lead to a party being timed-out.
+    ///      This is not a vulnerability as the arbitrator can rule in favor of one party anyway.
+    /// @param _transactionID The index of the transaction.
     function payArbitrationFeeBySender(uint256 _transactionID) external payable {
         Transaction storage transaction = transactions[_transactionID];
         require(
@@ -253,10 +251,9 @@ contract Escrow is IArbitrableV2 {
         }
     }
 
-    /** @dev Pay the arbitration fee to raise a dispute. To be called by the receiver. UNTRUSTED.
-     *  Note that this function mirrors payArbitrationFeeBySender.
-     *  @param _transactionID The index of the transaction.
-     */
+    /// @dev Pay the arbitration fee to raise a dispute. To be called by the receiver.
+    /// Note that this function mirrors payArbitrationFeeBySender.
+    /// @param _transactionID The index of the transaction.
     function payArbitrationFeeByReceiver(uint256 _transactionID) external payable {
         Transaction storage transaction = transactions[_transactionID];
         require(
@@ -282,9 +279,8 @@ contract Escrow is IArbitrableV2 {
         }
     }
 
-    /** @dev Reimburse sender if receiver fails to pay the fee.
-     *  @param _transactionID The index of the transaction.
-     */
+    /// @dev Reimburse sender if receiver fails to pay the fee.
+    /// @param _transactionID The index of the transaction.
     function timeOutBySender(uint256 _transactionID) external {
         Transaction storage transaction = transactions[_transactionID];
         require(transaction.status == Status.WaitingReceiver, "The transaction is not waiting on the receiver.");
@@ -298,9 +294,8 @@ contract Escrow is IArbitrableV2 {
         emit TransactionResolved(_transactionID, Resolution.TimeoutBySender);
     }
 
-    /** @dev Pay receiver if sender fails to pay the fee.
-     *  @param _transactionID The index of the transaction.
-     */
+    /// @dev Pay receiver if sender fails to pay the fee.
+    /// @param _transactionID The index of the transaction.
     function timeOutByReceiver(uint256 _transactionID) external {
         Transaction storage transaction = transactions[_transactionID];
         require(transaction.status == Status.WaitingSender, "The transaction is not waiting on the sender.");
@@ -315,10 +310,9 @@ contract Escrow is IArbitrableV2 {
         emit TransactionResolved(_transactionID, Resolution.TimeoutByReceiver);
     }
 
-    /** @dev Create a dispute. UNTRUSTED.
-     *  @param _transactionID The index of the transaction.
-     *  @param _arbitrationCost Amount to pay the arbitrator.
-     */
+    /// @dev Create a dispute.
+    /// @param _transactionID The index of the transaction.
+    /// @param _arbitrationCost Amount to pay the arbitrator.
     function raiseDispute(uint256 _transactionID, uint256 _arbitrationCost) internal {
         Transaction storage transaction = transactions[_transactionID];
         transaction.status = Status.DisputeCreated;
@@ -349,13 +343,11 @@ contract Escrow is IArbitrableV2 {
         }
     }
 
-    /** @dev Give a ruling for a dispute. Must be called by the arbitrator to
-     *  enforce the final ruling. The purpose of this function is to ensure that
-     *  the address calling it has the right to rule on the contract.
-     *  @param _disputeID ID of the dispute in the Arbitrator contract.
-     *  @param _ruling Ruling given by the arbitrator. Note that 0 is reserved
-     *  for "Refuse to arbitrate".
-     */
+    /// @dev Give a ruling for a dispute. Must be called by the arbitrator to enforce the final ruling.
+    ///      The purpose of this function is to ensure that the address calling it has the right to rule on the contract.
+    /// @param _disputeID ID of the dispute in the Arbitrator contract.
+    /// @param _ruling Ruling given by the arbitrator. Note that 0 is reserved
+    /// for "Refuse to arbitrate".
     function rule(uint256 _disputeID, uint256 _ruling) external override {
         require(msg.sender == address(arbitrator), "The caller must be the arbitrator.");
         require(_ruling <= AMOUNT_OF_CHOICES, "Invalid ruling.");
@@ -368,10 +360,9 @@ contract Escrow is IArbitrableV2 {
         executeRuling(transactionID, _ruling);
     }
 
-    /** @dev Execute a ruling of a dispute. It reimburses the fee to the winning party.
-     *  @param _transactionID The index of the transaction.
-     *  @param _ruling Ruling given by the arbitrator. 1 : Reimburse the receiver. 2 : Pay the sender.
-     */
+    /// @dev Execute a ruling of a dispute. It reimburses the fee to the winning party.
+    /// @param _transactionID The index of the transaction.
+    /// @param _ruling Ruling given by the arbitrator. 1 : Reimburse the receiver. 2 : Pay the sender.
     function executeRuling(uint256 _transactionID, uint256 _ruling) internal {
         Transaction storage transaction = transactions[_transactionID];
         // Give the arbitration fee back.
@@ -394,13 +385,12 @@ contract Escrow is IArbitrableV2 {
         emit TransactionResolved(_transactionID, Resolution.RulingEnforced);
     }
 
-    // **************************** //
-    // *     Constant getters     * //
-    // **************************** //
+    // ************************************* //
+    // *           Public Views            * //
+    // ************************************* //
 
-    /** @dev Getter to know the count of transactions.
-     *  @return The count of transactions.
-     */
+    /// @dev Getter to know the count of transactions.
+    /// @return The count of transactions.
     function getCountTransactions() external view returns (uint256) {
         return transactions.length;
     }
