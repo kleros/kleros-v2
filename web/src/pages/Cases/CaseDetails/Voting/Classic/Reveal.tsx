88aa8cf84579dcb15eb685b078e841dc2a0db71e
alcercu <333aleix333@gmail.com>
2024-01-09 16:14:46 +0100

    feat(web): show info messages when not in reveal period or when failed to commit

--- web/src/pages/Cases/CaseDetails/Voting/Classic/Reveal.tsx
+++ web/src/pages/Cases/CaseDetails/Voting/Classic/Reveal.tsx
@@ -7,17 +7,108 @@ import { useWalletClient, usePublicClient } from "wagmi";
 import ReactMarkdown from "react-markdown";
 import { Button } from "@kleros/ui-components-library";
 import { prepareWriteDisputeKitClassic } from "hooks/contracts/generated";
-import { wrapWithToast } from "utils/wrapWithToast";
-import { useDisputeDetailsQuery } from "queries/useDisputeDetailsQuery";
-import JustificationArea from "./JustificationArea";
-import { isUndefined } from "utils/index";
-import { useDisputeTemplate } from "queries/useDisputeTemplate";
 import useSigningAccount from "hooks/useSigningAccount";
+import { useDisputeDetailsQuery } from "queries/useDisputeDetailsQuery";
+import { useDisputeTemplate } from "queries/useDisputeTemplate";
+import { isUndefined } from "utils/index";
+import { wrapWithToast, catchShortMessage } from "utils/wrapWithToast";
+import InfoCard from "components/InfoCard";
+import JustificationArea from "./JustificationArea";
 
 const Container = styled.div`
   width: 100%;
   height: auto;
 `;
+
+const StyledInfoCard = styled(InfoCard)`
+  margin: 16px 0;
+`;
+
+const StyledButton = styled(Button)`
+  margin: 16px auto;
+`;
+
+interface IReveal {
+  arbitrable: `0x${string}`;
+  voteIDs: string[];
+  setIsOpen: (val: boolean) => void;
+  commit: string;
+  isRevealPeriod: boolean;
+}
+
+const Reveal: React.FC<IReveal> = ({ arbitrable, voteIDs, setIsOpen, commit, isRevealPeriod }) => {
+  const { id } = useParams();
+  const [isSending, setIsSending] = useState(false);
+  const parsedDisputeID = useMemo(() => BigInt(id ?? 0), [id]);
+  const parsedVoteIDs = useMemo(() => voteIDs.map((voteID) => BigInt(voteID)), [voteIDs]);
+  const { data: disputeData } = useDisputeDetailsQuery(id);
+  const [justification, setJustification] = useState("");
+  const { data: disputeTemplate } = useDisputeTemplate(id, arbitrable);
+  const { data: walletClient } = useWalletClient();
+  const publicClient = usePublicClient();
+  const { signingAccount, generateSigningAccount } = useSigningAccount();
+  const currentRoundIndex = disputeData?.dispute?.currentRoundIndex;
+  const saltKey = useMemo(
+    () => `dispute-${id}-round-${currentRoundIndex}-voteids-${voteIDs}`,
+    [id, currentRoundIndex, voteIDs]
+  );
+  const [storedSaltAndChoice, _] = useLocalStorage<string>(saltKey);
+
+  const handleReveal = useCallback(async () => {
+    setIsSending(true);
+    const { salt, choice } = isUndefined(storedSaltAndChoice)
+      ? await getSaltAndChoice(signingAccount, generateSigningAccount, saltKey, disputeTemplate.answers, commit)
+      : JSON.parse(storedSaltAndChoice);
+    if (isUndefined(choice)) return;
+    const { request } = await catchShortMessage(
+      prepareWriteDisputeKitClassic({
+        functionName: "castVote",
+        args: [parsedDisputeID, parsedVoteIDs, BigInt(choice), BigInt(salt), justification],
+      })
+    );
+    if (request && walletClient) {
+      await wrapWithToast(async () => await walletClient.writeContract(request), publicClient).then((result) => {
+        setIsOpen(result);
+      });
+    }
+    setIsSending(false);
+  }, [
+    commit,
+    disputeTemplate?.answers,
+    storedSaltAndChoice,
+    generateSigningAccount,
+    signingAccount,
+    saltKey,
+    justification,
+    parsedVoteIDs,
+    parsedDisputeID,
+    publicClient,
+    setIsOpen,
+    walletClient,
+  ]);
+
+  return (
+    <Container>
+      {isUndefined(commit) ? (
+        <StyledInfoCard msg="Failed to commit on time." />
+      ) : isRevealPeriod ? (
+        <>
+          <ReactMarkdown>{disputeTemplate?.question}</ReactMarkdown>
+          <JustificationArea {...{ justification, setJustification }} />
+          <StyledButton
+            variant="secondary"
+            text="Justify & Reveal"
+            disabled={isSending}
+            isLoading={isSending}
+            onClick={handleReveal}
+          />
+        </>
+      ) : (
+        <StyledInfoCard msg="Your vote was successfully commited, please wait until reveal period to reveal it." />
+      )}
+    </Container>
+  );
+};
 
 const getSaltAndChoice = async (
   signingAccount: PrivateKeyAccount | undefined,
@@ -46,79 +137,6 @@ const getSaltAndChoice = async (
     { found: false, choice: -1 }
   );
   return { salt, choice };
-};
-
-interface IReveal {
-  arbitrable: `0x${string}`;
-  voteIDs: string[];
-  setIsOpen: (val: boolean) => void;
-  commit: string;
-}
-
-const Reveal: React.FC<IReveal> = ({ arbitrable, voteIDs, setIsOpen, commit }) => {
-  const { id } = useParams();
-  const [isSending, setIsSending] = useState(false);
-  const parsedDisputeID = useMemo(() => BigInt(id ?? 0), [id]);
-  const parsedVoteIDs = useMemo(() => voteIDs.map((voteID) => BigInt(voteID)), [voteIDs]);
-  const { data: disputeData } = useDisputeDetailsQuery(id);
-  const [justification, setJustification] = useState("");
-  const { data: disputeTemplate } = useDisputeTemplate(id, arbitrable);
-  const { data: walletClient } = useWalletClient();
-  const publicClient = usePublicClient();
-  const { signingAccount, generateSigningAccount } = useSigningAccount();
-  const currentRoundIndex = disputeData?.dispute?.currentRoundIndex;
-  const saltKey = useMemo(
-    () => `dispute-${id}-round-${currentRoundIndex}-voteids-${voteIDs}`,
-    [id, currentRoundIndex, voteIDs]
-  );
-  const [storedSaltAndChoice, _] = useLocalStorage<string>(saltKey);
-
-  const handleReveal = useCallback(async () => {
-    setIsSending(true);
-    const { salt, choice } = isUndefined(storedSaltAndChoice)
-      ? await getSaltAndChoice(signingAccount, generateSigningAccount, saltKey, disputeTemplate.answers, commit)
-      : JSON.parse(storedSaltAndChoice);
-    if (isUndefined(choice)) return;
-    const { request } = await prepareWriteDisputeKitClassic({
-      functionName: "castVote",
-      args: [parsedDisputeID, parsedVoteIDs, BigInt(choice), BigInt(salt), justification],
-    });
-    if (request && walletClient) {
-      await wrapWithToast(async () => await walletClient.writeContract(request), publicClient).then((result) => {
-        setIsOpen(result);
-      });
-    }
-    setIsSending(false);
-  }, [
-    commit,
-    disputeTemplate?.answers,
-    storedSaltAndChoice,
-    generateSigningAccount,
-    signingAccount,
-    saltKey,
-    justification,
-    parsedVoteIDs,
-    parsedDisputeID,
-    publicClient,
-    setIsOpen,
-    walletClient,
-  ]);
-
-  return (
-    <Container>
-      <ReactMarkdown>{disputeTemplate.question}</ReactMarkdown>
-      <JustificationArea {...{ justification, setJustification }} />
-      <Button
-        variant="secondary"
-        text="Justify & Reveal"
-        disabled={isSending}
-        isLoading={isSending}
-        onClick={async () => {
-          handleReveal();
-        }}
-      />
-    </Container>
-  );
 };
 
 export default Reveal;
--- web/src/pages/Cases/CaseDetails/Voting/Classic/index.tsx
+++ web/src/pages/Cases/CaseDetails/Voting/Classic/index.tsx
@@ -17,15 +17,16 @@ const Classic: React.FC<IClassic> = ({ arbitrable, setIsOpen }) => {
   const { address } = useAccount();
   const { data: disputeData } = useDisputeDetailsQuery(id);
   const { data: drawData } = useDrawQuery(address?.toLowerCase(), id, disputeData?.dispute?.currentRound.id);
-  const isCommitReveal = useMemo(() => disputeData?.dispute?.court.hiddenVotes, [disputeData]);
+  const isHiddenVotes = useMemo(() => disputeData?.dispute?.court.hiddenVotes, [disputeData]);
+  const isCommitPeriod = useMemo(() => disputeData?.dispute?.period === "commit", [disputeData]);
   const commited = useMemo(() => drawData?.draws[0].vote?.commited, [drawData]);
   const commit = useMemo(() => drawData?.draws[0].vote?.commit, [drawData]);
   const voteIDs = useMemo(() => drawData?.draws?.map((draw) => draw.voteIDNum) as string[], [drawData]);
-  return id && isCommitReveal ? (
-    !commited ? (
+  return id && isHiddenVotes ? (
+    isCommitPeriod && !commited ? (
       <Commit {...{ arbitrable, setIsOpen, voteIDs }} />
     ) : (
-      <Reveal {...{ arbitrable, setIsOpen, voteIDs, commit }} />
+      <Reveal {...{ arbitrable, setIsOpen, voteIDs, commit, isRevealPeriod: !isCommitPeriod }} />
     )
   ) : (
     <Vote {...{ arbitrable, setIsOpen, voteIDs }} />
