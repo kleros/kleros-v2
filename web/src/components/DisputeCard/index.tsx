302d1db4c7f02c4bf83bbd04fdc70493ca9011a5
Harman-singh-waraich <Waraichharman68@gmail.com>
2024-01-22 16:40:14 +0530

    fix(web): truncate-long-titles-in-Dispute-info

--- web/src/components/DisputeCard/index.tsx
+++ web/src/components/DisputeCard/index.tsx
@@ -98,11 +98,6 @@ const DisputeCard: React.FC<IDisputeCard> = ({ id, arbitrated, period, lastPerio
       ? lastPeriodChange
       : getPeriodEndTimestamp(lastPeriodChange, currentPeriodIndex, court.timesPerPeriod);
   const { data: disputeTemplate } = useDisputeTemplate(id, arbitrated.id as `0x${string}`);
-  const title = isUndefined(disputeTemplate) ? (
-    <StyledSkeleton />
-  ) : (
-    disputeTemplate?.title ?? "The dispute's template is not correct please vote refuse to arbitrate"
-  );
   const { data: courtPolicy } = useCourtPolicy(court.id);
   const courtName = courtPolicy?.name;
   const category = disputeTemplate ? disputeTemplate.category : undefined;
@@ -115,7 +110,14 @@ const DisputeCard: React.FC<IDisputeCard> = ({ id, arbitrated, period, lastPerio
         <StyledCard hover onClick={() => navigate(`/cases/${id.toString()}`)}>
           <PeriodBanner id={parseInt(id)} period={currentPeriodIndex} />
           <CardContainer>
-            <h3>{title}</h3>
+            {isUndefined(disputeTemplate) ? (
+              <StyledSkeleton />
+            ) : (
+              <TruncatedTitle
+                text={disputeTemplate?.title ?? "The dispute's template is not correct please vote refuse to arbitrate"}
+                maxLength={100}
+              />
+            )}
             <DisputeInfo
               courtId={court?.id}
               court={courtName}
