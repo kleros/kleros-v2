255338a4a645ac4b90dd677d938bcee59c213b44
Harman-singh-waraich <Waraichharman68@gmail.com>
2024-01-23 12:39:42 +0530

    feat(web): show-unit-in-Number-dislpay-tooltip

--- web/src/components/NumberDisplay.tsx
+++ web/src/components/NumberDisplay.tsx
@@ -2,16 +2,30 @@ import { Tooltip } from "@kleros/ui-components-library";
 import React from "react";
 interface INumberDisplay {
   value: string | number;
+  unit?: string;
+  showUnitInDisplay?: boolean;
   decimals?: number;
   place?: "bottom" | "top" | "left" | "right";
+  isCurrency?: boolean; //currency units are shown in front
 }
 
-const NumberDisplay: React.FC<INumberDisplay> = ({ value, place, decimals = 2 }) => {
+const NumberDisplay: React.FC<INumberDisplay> = ({
+  value,
+  unit,
+  place,
+  decimals = 2,
+  showUnitInDisplay = true,
+  isCurrency = false,
+}) => {
   const parsedValue = Number(value);
   const formattedValue = parsedValue % 1 !== 0 ? parsedValue.toFixed(decimals) : parsedValue.toFixed(0);
+  const tooltipValue = isCurrency ? `${unit} ${value}` : `${value} ${unit}`;
+  const displayValue = isCurrency
+    ? `${showUnitInDisplay ? unit : ""} ${formattedValue}`
+    : `${formattedValue} ${showUnitInDisplay ? unit : ""}`;
   return (
-    <Tooltip small text={value.toString()} place={place}>
-      {formattedValue}
+    <Tooltip small text={tooltipValue} place={place}>
+      {displayValue}
     </Tooltip>
   );
 };
