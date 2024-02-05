62197ad2fd710a37c9cb5c448854d3d880dfbd24
Harman-singh-waraich <Waraichharman68@gmail.com>
2024-02-05 19:29:05 +0530

    fix(web): fix-dispute-request-fetch-from-txn-hash

--- web/src/utils/getDisputeRequestParamsFromTxn.ts
+++ web/src/utils/getDisputeRequestParamsFromTxn.ts
@@ -1,6 +1,7 @@
-import { GetTransactionReceiptReturnType, decodeEventLog } from "viem";
+import { GetTransactionReceiptReturnType, decodeEventLog, getEventSelector, parseAbi } from "viem";
 import { getPublicClient } from "wagmi/dist/actions";
 import { iArbitrableV2ABI } from "hooks/contracts/generated";
+import { isUndefined } from ".";
 
 export const getDisputeRequestParamsFromTxn = async (hash: `0x${string}`, chainId: number) => {
   try {
@@ -10,16 +11,20 @@ export const getDisputeRequestParamsFromTxn = async (hash: `0x${string}`, chainI
       hash,
     });
 
+    const selector = getEventSelector("DisputeRequest(address,uint256,uint256,uint256,string)");
+    const disputeRequestEvent = txn.logs.find((log) => log.topics[0] === selector);
+    if (isUndefined(disputeRequestEvent)) return undefined;
+
     const topics = decodeEventLog({
       abi: iArbitrableV2ABI,
       eventName: "DisputeRequest",
-      topics: txn.logs[2].topics,
-      data: txn.logs[2].data,
+      topics: disputeRequestEvent?.topics,
+      data: disputeRequestEvent?.data,
     });
 
     return {
       ...topics?.args,
-      _arbitrable: txn.logs[2].address,
+      _arbitrable: disputeRequestEvent.address,
     };
   } catch (e) {
     console.log("getDisputeRequestParamsFromTxn :", e);
