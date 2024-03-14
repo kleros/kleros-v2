7d7fb3bf7fd4b7eec02962e9fbb8a5a6494feeb3
jaybuidl <jb@kleros.io>
2024-03-14 17:18:49 +0000

    fix: minor

--- web/src/consts/index.ts
+++ web/src/consts/index.ts
@@ -26,14 +26,14 @@ export const TELEGRAM_REGEX = /^@\w{5,32}$/;
 export const ETH_ADDRESS_REGEX = /^0x[a-fA-F0-9]{40}$/;
 export const ETH_SIGNATURE_REGEX = /^0x[a-fA-F0-9]{130}$/;
 
-export const GENESIS_BLOCK_ARBSEPOLIA = BigInt(process.env.REACT_APP_GENESIS_BLOCK_ARBSEPOLIA ?? 0);
-export const GENESIS_BLOCK_ARBMAINNET = BigInt(process.env.REACT_APP_GENESIS_BLOCK_ARBMAINNET ?? 0);
-export const genesisBlock = () => (isProductionDeployment() ? GENESIS_BLOCK_ARBMAINNET : GENESIS_BLOCK_ARBSEPOLIA);
-
 export const isProductionDeployment = () => process.env.REACT_APP_DEPLOYMENT === "mainnet";
 export const isUniversityCourt = () => process.env.REACT_APP_UNIVERSITY_COURT?.toLowerCase() === "true";
 export const isNeoCourt = () => process.env.REACT_APP_NEO_COURT?.toLowerCase() === "true";
 export const whichCourt = () => (isUniversityCourt() ? COURTS.university : isNeoCourt() ? COURTS.neo : COURTS.vanilla);
+
+export const GENESIS_BLOCK_ARBSEPOLIA = BigInt(process.env.REACT_APP_GENESIS_BLOCK_ARBSEPOLIA ?? 0);
+export const GENESIS_BLOCK_ARBMAINNET = BigInt(process.env.REACT_APP_GENESIS_BLOCK_ARBMAINNET ?? 0);
+export const genesisBlock = () => (isProductionDeployment() ? GENESIS_BLOCK_ARBMAINNET : GENESIS_BLOCK_ARBSEPOLIA);
 
 export const INVALID_DISPUTE_DATA_ERROR = `The dispute data is not valid, please vote "Refuse to arbitrate"`;
 export const RPC_ERROR = `RPC Error: Unable to fetch dispute data. Please avoid voting.`;
--- web/src/utils/getGraphqlUrl.ts
+++ web/src/utils/getGraphqlUrl.ts
@@ -3,12 +3,12 @@ import { DEFAULT_CHAIN } from "src/consts/chains";
 
 const CHAINID_TO_DISPUTE_TEMPLATE_SUBGRAPH = {
   [arbitrumSepolia.id]:
-    process.env.REACT_APP_DRT_ARBSEPOLIA_SUBGRAPH ?? "Wrong Subgraph URL. Please check the environment variables.",
+    process.env.REACT_APP_DRT_ARBSEPOLIA_SUBGRAPH ?? "Environment variables REACT_APP_DRT_ARBSEPOLIA_SUBGRAPH not set.",
   [arbitrum.id]:
-    process.env.REACT_APP_DRT_ARBMAINNET_SUBGRAPH ?? "Wrong Subgraph URL. Please check the environment variables.",
+    process.env.REACT_APP_DRT_ARBMAINNET_SUBGRAPH ?? "Environment variables REACT_APP_DRT_ARBMAINNET_SUBGRAPH not set.",
 };
 
 export const getGraphqlUrl = (isDisputeTemplate = false, chainId: number = DEFAULT_CHAIN) => {
-  const coreUrl = process.env.REACT_APP_CORE_SUBGRAPH ?? "Wrong Subgraph URL. Please check the environment variables.";
+  const coreUrl = process.env.REACT_APP_CORE_SUBGRAPH ?? "Environment variables REACT_APP_CORE_SUBGRAPH not set.";
   return isDisputeTemplate ? CHAINID_TO_DISPUTE_TEMPLATE_SUBGRAPH[chainId] : coreUrl;
 };
