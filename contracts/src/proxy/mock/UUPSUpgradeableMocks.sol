a852eae9167c8b5f812aa1ab21462c954a1c3db2
anmol-dhiman <anmoldhiman7111@gmail.com>
2024-04-09 16:09:12 +0530

    chore(proxy): initializable storage formual and mock proxy migration

--- contracts/src/proxy/mock/UUPSUpgradeableMocks.sol
+++ contracts/src/proxy/mock/UUPSUpgradeableMocks.sol
@@ -7,43 +7,70 @@ import "../UUPSProxiable.sol";
 import "../Initializable.sol";
 
 contract NonUpgradeableMock {
-    uint256 public _counter;
+    /// @custom:storage-location erc7201:kleros.storage.NonUpgradeableMockStorage
+    struct NonUpgradeableMockStorage {
+        uint256 counter;
+    }
+    // keccak256(abi.encode(uint256(keccak256("kleros.storage.NonUpgradeableMockStorage")) - 1)) & ~bytes32(uint256(0xff))
+    bytes32 private constant INITIALIZABLE_STORAGE = 0x203890a1e96eaf39c35df205fbcedb580d4552ae7ac40780014dc281a7acd500;
 
     function counter() external view returns (uint256) {
-        return _counter;
+        NonUpgradeableMockStorage storage $ = _getNonUpgradeableMockStorage();
+        return $.counter;
     }
 
     function increment() external {
-        _counter++;
+        NonUpgradeableMockStorage storage $ = _getNonUpgradeableMockStorage();
+        $.counter++;
     }
 
     function version() external pure virtual returns (string memory) {
         return "NonUpgradeableMock 0.0.0";
     }
+    /**
+     * @dev Returns a pointer to the storage namespace.
+     */
+    // solhint-disable-next-line var-name-mixedcase
+    function _getNonUpgradeableMockStorage() private pure returns (NonUpgradeableMockStorage storage $) {
+        assembly {
+            $.slot := INITIALIZABLE_STORAGE
+        }
+    }
 }
 
 contract UUPSUpgradeableMock is UUPSProxiable, NonUpgradeableMock {
-    bool public initialized;
-    address public governor;
-
-    uint256[50] __gap;
-
-    constructor() {
-        initialized = true;
+    /// @custom:storage-location erc7201:kleros.storage.UUPSUpgradeableMock
+    struct UUPSUpgradeableMockStorage {
+        bool initialized;
+        address governor;
     }
 
+    // keccak256(abi.encode(uint256(keccak256("kleros.storage.UUPSUpgradeableMock")) - 1)) & ~bytes32(uint256(0xff))
+    bytes32 private constant INITIALIZABLE_STORAGE = 0x30d370aa3c43b12c906d59e729739fe781988b5aeb55fa9fb184b45129d6f900;
+
     function initialize(address _governor) external {
-        require(!initialized, "Contract instance has already been initialized");
-        governor = _governor;
-        initialized = true;
+        UUPSUpgradeableMockStorage storage $ = _getInitializableStorage();
+        require(!$.initialized, "Contract instance has already been initialized");
+        $.governor = _governor;
+        $.initialized = true;
     }
 
     function _authorizeUpgrade(address) internal view override {
-        require(governor == msg.sender, "No privilege to upgrade");
+        UUPSUpgradeableMockStorage storage $ = _getInitializableStorage();
+        require($.governor == msg.sender, "No privilege to upgrade");
     }
 
     function version() external pure virtual override returns (string memory) {
         return "UUPSUpgradeableMock 1.0.0";
+    }
+    /**
+     * @dev Returns a pointer to the storage namespace.
+     */
+    // solhint-disable-next-line var-name-mixedcase
+    function _getInitializableStorage() private pure returns (UUPSUpgradeableMockStorage storage $) {
+        assembly {
+            $.slot := INITIALIZABLE_STORAGE
+        }
     }
 }
 
