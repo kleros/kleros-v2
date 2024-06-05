a4640f50ae13e8f70ac5133a5d43908fd4d2d2ef
alcercu <333aleix333@gmail.com>
2024-06-05 15:44:24 +0200

    fix(web): config only needed polyfills

--- web/vite.config.js
+++ web/vite.config.js
@@ -24,6 +24,8 @@ export default defineConfig({
     tsconfigPaths({
       ignoreConfigErrors: true,
     }),
-    nodePolyfills(),
+    nodePolyfills({
+      include: ["fs", "stream"],
+    }),
   ],
 });
