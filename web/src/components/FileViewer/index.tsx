8010fc1f09213e6d41227cb408b8205d9e1f2dcc
Harman-singh-waraich <Waraichharman68@gmail.com>
2024-06-13 18:30:22 +0530

    feat(web): custom-scrollbar

--- web/src/components/FileViewer/index.tsx
+++ web/src/components/FileViewer/index.tsx
@@ -2,12 +2,11 @@ import React from "react";
 import styled from "styled-components";
 
 import DocViewer, { DocViewerRenderers } from "@cyntler/react-doc-viewer";
+
 import "@cyntler/react-doc-viewer/dist/index.css";
-import { OverlayScrollbarsComponent } from "overlayscrollbars-react";
+import { customScrollbar } from "styles/customScrollbar";
 
 import MarkdownRenderer from "./Viewers/MarkdownViewer";
-
-import "overlayscrollbars/styles/overlayscrollbars.css";
 
 const Wrapper = styled.div`
   background-color: ${({ theme }) => theme.whiteBackground};
@@ -15,6 +14,8 @@ const Wrapper = styled.div`
   box-shadow: 0px 2px 3px 0px rgba(0, 0, 0, 0.06);
   max-height: 750px;
   overflow: scroll;
+
+  ${customScrollbar}
 `;
 
 const StyledDocViewer = styled(DocViewer)`
@@ -29,25 +30,23 @@ const StyledDocViewer = styled(DocViewer)`
 const FileViewer: React.FC<{ url: string }> = ({ url }) => {
   const docs = [{ uri: url }];
   return (
-    <OverlayScrollbarsComponent options={{ showNativeOverlaidScrollbars: true }}>
-      <Wrapper className="file-viewer-wrapper">
-        <StyledDocViewer
-          documents={docs}
-          pluginRenderers={[...DocViewerRenderers, MarkdownRenderer]}
-          config={{
-            header: {
-              disableHeader: true,
-              disableFileName: true,
-            },
-            pdfZoom: {
-              defaultZoom: 0.8,
-              zoomJump: 0.1,
-            },
-            pdfVerticalScrollByDefault: true, // false as default
-          }}
-        />
-      </Wrapper>
-    </OverlayScrollbarsComponent>
+    <Wrapper className="file-viewer-wrapper">
+      <StyledDocViewer
+        documents={docs}
+        pluginRenderers={[...DocViewerRenderers, MarkdownRenderer]}
+        config={{
+          header: {
+            disableHeader: true,
+            disableFileName: true,
+          },
+          pdfZoom: {
+            defaultZoom: 0.8,
+            zoomJump: 0.1,
+          },
+          pdfVerticalScrollByDefault: true, // false as default
+        }}
+      />
+    </Wrapper>
   );
 };
 
