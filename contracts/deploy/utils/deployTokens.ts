13a89316b3ef8a78cfbd474154ba7d0e5ff81922
anmol-dhiman <anmoldhiman7111@gmail.com>
2024-06-17 23:32:52 +0530

    chore: deploy utils refactor

--- contracts/deploy/utils/deployTokens.ts
+++ contracts/deploy/utils/deployTokens.ts
@@ -1,6 +1,6 @@
-import { Contract } from "@ethersproject/contracts";
+import { Contract } from "ethers";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
-import { BigNumber } from "ethers";
+import { toBigInt } from "ethers";
 import { getContractOrDeploy } from "./getContractOrDeploy";
 import { isMainnet } from ".";
 
@@ -8,7 +8,7 @@ export const deployERC20AndFaucet = async (
   hre: HardhatRuntimeEnvironment,
   deployer: string,
   ticker: string,
-  faucetFundingAmount: BigNumber = hre.ethers.utils.parseUnits("100000")
+  faucetFundingAmount: bigint = hre.ethers.parseUnits("100000")
 ): Promise<Contract> => {
   const erc20 = await deployERC20(hre, deployer, ticker);
   if (!isMainnet(hre.network)) {
@@ -35,7 +35,7 @@ export const deployFaucet = async (
   deployer: string,
   ticker: string,
   erc20: Contract,
-  faucetFundingAmount: BigNumber
+  faucetFundingAmount: bigint
 ): Promise<void> => {
   const faucet = await getContractOrDeploy(hre, `${ticker}Faucet`, {
     from: deployer,
@@ -45,7 +45,7 @@ export const deployFaucet = async (
   });
   const faucetBalance = await erc20.balanceOf(faucet.address);
   const deployerBalance = await erc20.balanceOf(deployer);
-  if (deployerBalance.gte(faucetFundingAmount) && faucetBalance.lt(faucetFundingAmount.div(5))) {
+  if (deployerBalance.gte(faucetFundingAmount) && faucetBalance.lt(faucetFundingAmount / toBigInt(5))) {
     // Fund the faucet if deployer has enough tokens and if the faucet has less than 20% of the faucetFundingAmount
     console.log(`funding ${ticker}Faucet with ${faucetFundingAmount}`);
     await erc20.transfer(faucet.address, faucetFundingAmount);
--- contracts/deploy/utils/getContractOrDeploy.ts
+++ contracts/deploy/utils/getContractOrDeploy.ts
@@ -1,18 +1,17 @@
-import { Contract } from "@ethersproject/contracts";
 import { DeployOptions } from "hardhat-deploy/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { deployUpgradable } from "./deployUpgradable";
-
+import { Contract } from "ethers";
 export const getContractOrDeploy = async (
   hre: HardhatRuntimeEnvironment,
   contractName: string,
   options: DeployOptions
 ): Promise<Contract> => {
-  let contract = await hre.ethers.getContractOrNull(contractName);
+  let contract = await hre.ethers.getContractOrNull<Contract>(contractName);
   if (!contract) {
     console.log(`contract ${contractName} not deployed, deploying now...`);
     await hre.deployments.deploy(contractName, options);
-    contract = await hre.ethers.getContract(contractName);
+    contract = await hre.ethers.getContract<Contract>(contractName);
   } else {
     console.log(`contract ${contractName} already deployed`);
   }
@@ -24,11 +23,11 @@ export const getContractOrDeployUpgradable = async (
   contractName: string,
   options: DeployOptions
 ): Promise<Contract> => {
-  let contract = await hre.ethers.getContractOrNull(contractName);
+  let contract = await hre.ethers.getContractOrNull<Contract>(contractName);
   if (!contract) {
     console.log(`contract ${contractName} not deployed, deploying as upgradable now...`);
     await deployUpgradable(hre.deployments, contractName, options);
-    contract = await hre.ethers.getContract(contractName);
+    contract = await hre.ethers.getContract<Contract>(contractName);
   } else {
     console.log(`contract ${contractName} already deployed`);
   }
