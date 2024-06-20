078c4ca936a379542d633f0e754dd8cd09c161a8
kemuru <102478601+kemuru@users.noreply.github.com>
2024-06-20 19:20:52 +0200

    chore: revert changes to jsonaction and replaceplaceholderswithvalues

--- kleros-sdk/src/dataMappings/actions/jsonAction.ts
+++ kleros-sdk/src/dataMappings/actions/jsonAction.ts
@@ -3,7 +3,5 @@ import { createResultObject } from "src/dataMappings/utils/createResultObject";
 
 export const jsonAction = (mapping: JsonMapping) => {
   const { value: source, seek, populate } = mapping;
-  const sourceData = JSON.parse(source);
-
-  return createResultObject(sourceData, seek, populate);
+  return createResultObject(source, seek, populate);
 };
--- kleros-sdk/src/dataMappings/utils/replacePlaceholdersWithValues.ts
+++ kleros-sdk/src/dataMappings/utils/replacePlaceholdersWithValues.ts
@@ -3,18 +3,11 @@ export const replacePlaceholdersWithValues = (mapping: any, context: any) => {
 
   const replacedMapping = mappingAsString.replace(/\{\{(\w+)\}\}/g, (_, variableName) => {
     if (context.hasOwnProperty(variableName)) {
-      const value = context[variableName];
-
-      if (typeof value === "string" && value.startsWith("{") && value.endsWith("}")) {
-        return JSON.stringify(value);
-      } else {
-        return typeof value === "bigint" ? value.toString() : value;
-      }
+      return context[variableName];
     } else {
       throw new Error(`Variable '${variableName}' not found in context.`);
     }
   });
 
-  const parsedMapping = JSON.parse(replacedMapping.replace(/"(\{.*\})"/, "$1"));
-  return parsedMapping;
+  return JSON.parse(replacedMapping);
 };
