8404104852f1673d583f4e62f61e82df8744c796
kemuru <102478601+kemuru@users.noreply.github.com>
2024-06-25 08:47:34 +0200

    feat: add externaldisputeid as context to the dispute too

--- web/src/hooks/queries/usePopulatedDisputeData.ts
+++ web/src/hooks/queries/usePopulatedDisputeData.ts
@@ -9,6 +9,7 @@ import { populateTemplate } from "@kleros/kleros-sdk/src/dataMappings/utils/popu
 import { GENESIS_BLOCK_ARBSEPOLIA } from "consts/index";
 import { useGraphqlBatcher } from "context/GraphqlBatcher";
 import { iArbitrableV2Abi } from "hooks/contracts/generated";
+import { useEvidenceGroup } from "queries/useEvidenceGroup";
 import { debounceErrorToast } from "utils/debounceErrorToast";
 import { isUndefined } from "utils/index";
 
@@ -32,6 +33,7 @@ export const usePopulatedDisputeData = (disputeID?: string, arbitrableAddress?: 
   const { data: crossChainData, isError } = useIsCrossChainDispute(disputeID, arbitrableAddress);
   const isEnabled = !isUndefined(disputeID) && !isUndefined(crossChainData) && !isUndefined(arbitrableAddress);
   const { graphqlBatcher } = useGraphqlBatcher();
+  const externalDisputeID = useEvidenceGroup(disputeID, arbitrableAddress);
 
   return useQuery<DisputeDetails>({
     queryKey: [`DisputeTemplate${disputeID}${arbitrableAddress}`],
@@ -60,6 +62,7 @@ export const usePopulatedDisputeData = (disputeID?: string, arbitrableAddress?: 
             disputeID: disputeID,
             arbitrable: arbitrableAddress,
             graphApiKey: process.env.GRAPH_API_KEY,
+            externalDisputeID: externalDisputeID,
           };
 
           const data = dataMappings ? await executeActions(JSON.parse(dataMappings), initialContext) : {};
