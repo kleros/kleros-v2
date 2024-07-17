5a9f7324afbf556fe876715e3db4c6fdf5ef9c55
Nikhil <nv5475609@gmail.com>
2024-07-17 14:50:02 +0530

    refactor(devtools): dynamic import graphql-url from env

--- web-devtools/src/utils/getGraphqlUrl.ts
+++ web-devtools/src/utils/getGraphqlUrl.ts
@@ -4,9 +4,13 @@ import { DEFAULT_CHAIN } from "../consts/chains";
 
 export const getGraphqlUrl = (isDisputeTemplate = false, chainId: number = DEFAULT_CHAIN) => {
   const CHAINID_TO_DISPUTE_TEMPLATE_SUBGRAPH: { [key: number]: string } = {
-    [arbitrumSepolia.id]: "https://api.studio.thegraph.com/query/61738/kleros-v2-drt-arbisep-devnet/version/latest",
-    [arbitrum.id]: "https://api.studio.thegraph.com/query/61738/kleros-v2-drt-arbisep-devnet/version/latest",
+    [arbitrumSepolia.id]:
+      process.env.NEXT_PUBLIC_DRT_ARBSEPOLIA_SUBGRAPH ??
+      "Environment variable NEXT_PUBLIC_DRT_ARBSEPOLIA_SUBGRAPH not set.",
+    [arbitrum.id]:
+      process.env.NEXT_PUBLIC_DRT_ARBMAINNET_SUBGRAPH ??
+      "Environment variable NEXT_PUBLIC_DRT_ARBMAINNET_SUBGRAPH not set.",
   };
-  const coreUrl = "https://api.studio.thegraph.com/query/61738/kleros-v2-core-devnet/version/latest";
+  const coreUrl = process.env.NEXT_PUBLIC_CORE_SUBGRAPH ?? "Environment variables NEXT_PUBLIC_CORE_SUBGRAPH not set.";
   return isDisputeTemplate ? CHAINID_TO_DISPUTE_TEMPLATE_SUBGRAPH[chainId] : coreUrl;
 };
