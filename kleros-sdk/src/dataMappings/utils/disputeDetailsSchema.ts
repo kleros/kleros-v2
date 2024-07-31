586c4c13bc71f879c37fe5fa0ffea2716d553952
kemuru <102478601+kemuru@users.noreply.github.com>
2024-07-31 12:35:35 +0200

    feat: multiaddr check for policy-uri, arbitratoraddress as ethaddressschema

--- kleros-sdk/src/dataMappings/utils/disputeDetailsSchema.ts
+++ kleros-sdk/src/dataMappings/utils/disputeDetailsSchema.ts
@@ -3,8 +3,11 @@ import { isAddress } from "viem";
 import { normalize } from "viem/ens";
 
 const isHexAddress = (str: string): boolean => /^0x[a-fA-F0-9]{40}$/.test(str);
-
 const isHexId = (str: string): boolean => /^0x[a-fA-F0-9]{1,64}$/.test(str);
+const isMultiaddr = (str: string): boolean =>
+  /^\/(?:ip4|ip6|dns4|dns6|dnsaddr|tcp|udp|utp|tls|ws|wss|ipfs|p2p-circuit|p2p-webrtc-star|p2p-webrtc-direct|p2p-websocket-star|onion)(\/[^\s\/]+)*$/.test(
+    str
+  );
 
 export const ethAddressSchema = z.string().refine((value) => isAddress(value), {
   message: "Provided address is invalid.",
@@ -33,7 +36,7 @@ export const AnswerSchema = z.object({
   id: z
     .string()
     .regex(/^0x[0-9a-fA-F]+$/)
-    .optional(), // should be a bigint
+    .optional(),
   title: z.string(),
   description: z.string(),
   reserved: z.boolean().optional(),
@@ -53,12 +56,14 @@ const DisputeDetailsSchema = z.object({
   description: z.string(),
   question: z.string(),
   answers: z.array(AnswerSchema),
-  policyURI: z.string(),
+  policyURI: z.string().refine((value) => isMultiaddr(value), {
+    message: "Provided policy URI is not a valid multiaddr.",
+  }),
   attachment: AttachmentSchema.optional(),
   frontendUrl: z.string().optional(),
   metadata: MetadataSchema.optional(),
   arbitratorChainID: z.string(),
-  arbitratorAddress: z.string(), // should be changed for ethAddressSchema eventually, but some
+  arbitratorAddress: ethAddressSchema,
   category: z.string().optional(),
   lang: z.string().optional(),
   specification: z.string().optional(),
