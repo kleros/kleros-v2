2c628868c02eab77d0ff084d9c3a42f8f26b10e6
Harman-singh-waraich <Waraichharman68@gmail.com>
2024-10-04 17:57:08 +0530

    refactor(web-devtools): update-labeled-input-style

--- web-devtools/src/components/LabeledInput.tsx
+++ web-devtools/src/components/LabeledInput.tsx
@@ -1,38 +1,72 @@
-import React from "react";
-import styled from "styled-components";
+import React, { useRef } from "react";
+import styled, { css } from "styled-components";
 
 import { Checkbox, Field, type CheckboxProps, type FieldProps } from "@kleros/ui-components-library";
 
 const Container = styled.div`
   display: flex;
-  max-width: 300px;
-  min-width: 250px;
-  height: 50px;
+  align-items: center;
+  width: 280px;
+  max-width: 280px;
+  height: 46px;
+  position: relative;
+  box-sizing: border-box;
 `;
 
-const LabelContainer = styled.div`
+const ContainerCss = css`
   flex: 1;
+  height: 100%;
   display: flex;
   align-items: center;
   justify-content: center;
   border: 1px solid ${({ theme }) => theme.klerosUIComponentsStroke};
-  border-radius: 6px 0px 0px 6px;
+`;
+
+const LabelContainer = styled.div<{ isField?: boolean }>`
+  ${ContainerCss}
+  border-radius: 3px 0px 0px 3px;
+  background-color: ${({ theme }) => theme.klerosUIComponentsLightBackground};
+  ${({ isField }) =>
+    isField &&
+    css`
+      width: 50%;
+      height: 45px;
+      position: absolute;
+      top: 0.5px;
+      left: 0.5px;
+      z-index: 1;
+    `}
 `;
 
 const Label = styled.label`
   color: ${({ theme }) => theme.klerosUIComponentsPrimaryText};
 `;
 
-const InputContainer = styled(LabelContainer)`
+const InputContainer = styled.div<{ isField?: boolean }>`
+  ${ContainerCss}
   position: relative;
-  border-radius: 0px 6px 6px 0px;
+  border-radius: 0px 3px 3px 0px;
   border-left: none;
+  background-color: ${({ theme }) => theme.klerosUIComponentsWhiteBackground};
+  ${({ isField }) =>
+    isField &&
+    css`
+      width: 100%;
+      z-index: 0;
+      border-radius: 3px;
+    `}
 `;
 
-const StyledField = styled(Field)`
-  width: auto;
+const StyledField = styled(Field)<{ paddingLeft?: number }>`
+  width: 100%;
   > input {
     border: none;
+    box-sizing: border-box;
+    ${({ paddingLeft }) =>
+      paddingLeft &&
+      css`
+        padding-left: ${paddingLeft + 8}px;
+      `}
   }
 `;
 
@@ -53,13 +87,14 @@ type DefaultInputProps = FieldProps & { inputType?: "field" };
 type LabeledInputProps = BaseProps & (CheckboxInputProps | DefaultInputProps);
 
 const LabeledInput: React.FC<LabeledInputProps> = ({ inputType = "field", label, ...props }) => {
+  const labelRef = useRef<HTMLDivElement>(null);
   return (
     <Container>
-      <LabelContainer>
+      <LabelContainer ref={labelRef} isField={inputType === "field"}>
         <Label>{label}</Label>
       </LabelContainer>
-      <InputContainer>
-        {inputType === "field" && <StyledField {...props} />}
+      <InputContainer isField={inputType === "field"}>
+        {inputType === "field" && <StyledField {...props} paddingLeft={labelRef.current?.offsetWidth} />}
         {inputType === "checkbox" && <StyledCheckbox label="&nbsp;" {...props} />}
       </InputContainer>
     </Container>
