7faeef3dd595beb86a65ee4f6cd7a782061db74f
jaybuidl <jb@kleros.io>
2024-10-11 01:19:39 +0100

    fix: getGraphqlUrl error handling without dynamic env var keys

--- web-devtools/src/utils/getGraphqlUrl.ts
+++ web-devtools/src/utils/getGraphqlUrl.ts
@@ -2,23 +2,24 @@ import { arbitrumSepolia, arbitrum } from "wagmi/chains";
 
 import { DEFAULT_CHAIN } from "../consts/chains";
 
-function assertEnvVar(key: string): string {
-  const value = process.env[key];
-  if (!value) {
-    throw new Error(`${key} not set`);
-  }
+const throwIfNotSet = (key: string, value: string | undefined) => {
+  if (!value) throw new Error(`${key} not set.`);
   return value;
-}
+};
 
-export function getGraphqlUrl(isDisputeTemplate = false, chainId: number = DEFAULT_CHAIN) {
-  const coreSubgraphEnvVar = "NEXT_PUBLIC_CORE_SUBGRAPH";
-  const drtArbSepoliaSubgraphEnvVar = "NEXT_PUBLIC_DRT_ARBSEPOLIA_SUBGRAPH";
-  const drtArbMainnetSubgraphEnvVar = "NEXT_PUBLIC_DRT_ARBMAINNET_SUBGRAPH";
-
-  const chainIdToDrtSubgraph: { [key: number]: string } = {
-    [arbitrumSepolia.id]: drtArbSepoliaSubgraphEnvVar,
-    [arbitrum.id]: drtArbMainnetSubgraphEnvVar,
-  };
-
-  return assertEnvVar(isDisputeTemplate ? chainIdToDrtSubgraph[chainId] : coreSubgraphEnvVar);
-}
+// Warning: we cannot access process.env[key] with key a variable
+// because of the way Next.js handles env variables, unless hacking next.config
+// More info: https://stackoverflow.com/questions/64152943/cant-access-process-env-values-using-dynamic-keys
+export const getGraphqlUrl = (isDisputeTemplate = false, chainId: number = DEFAULT_CHAIN) => {
+  if (!isDisputeTemplate) {
+    return throwIfNotSet("NEXT_PUBLIC_CORE_SUBGRAPH", process.env.NEXT_PUBLIC_CORE_SUBGRAPH);
+  }
+  switch (chainId) {
+    case arbitrumSepolia.id:
+      return throwIfNotSet("NEXT_PUBLIC_DRT_ARBSEPOLIA_SUBGRAPH", process.env.NEXT_PUBLIC_DRT_ARBSEPOLIA_SUBGRAPH);
+    case arbitrum.id:
+      return throwIfNotSet("NEXT_PUBLIC_DRT_ARBMAINNET_SUBGRAPH", process.env.NEXT_PUBLIC_DRT_ARBMAINNET_SUBGRAPH);
+    default:
+      throw new Error(`Unsupported chainId: ${chainId}`);
+  }
+};
