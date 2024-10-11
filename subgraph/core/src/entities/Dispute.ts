cdbbcf2d06ac4e51ca076e9d23e0ef8afc938013
Harman-singh-waraich <Waraichharman68@gmail.com>
2024-10-11 10:55:44 +0530

    feat(subgraph): fetch-dispute-request-event-data-within-subgraph

--- subgraph/core/src/entities/Dispute.ts
+++ subgraph/core/src/entities/Dispute.ts
@@ -1,3 +1,4 @@
+import { BigInt, ByteArray, crypto, dataSource, ethereum } from "@graphprotocol/graph-ts";
 import { KlerosCore, DisputeCreation } from "../../generated/KlerosCore/KlerosCore";
 import { Court, Dispute } from "../../generated/schema";
 import { ZERO } from "../utils";
@@ -27,4 +28,73 @@ export function createDisputeFromEvent(event: DisputeCreation): void {
   const roundID = `${disputeID.toString()}-${ZERO.toString()}`;
   dispute.currentRound = roundID;
   dispute.save();
+
+  updateDisputeRequestData(event);
+}
+
+const DisputeRequest = "DisputeRequest(address,uint256,uint256,uint256,string)";
+
+const DisputeRequestSignature = crypto.keccak256(ByteArray.fromUTF8(DisputeRequest));
+
+// note : we are using bytes32 in place of string as strings cannot be decoded and it breaks the function.
+// It is okay for us, as we are interested in the uint256 in front
+// _externalDisputeId,_templateId,_tenplateUri
+const DisputeRequestTypestring = "(uint256,uint256,bytes32)";
+
+const CrossChainDisputeIncoming =
+  "CrossChainDisputeIncoming(address,uint256,address,uint256,uint256,uint256,uint256,string)";
+
+const CrossChainDisputeIncomingSignature = crypto.keccak256(ByteArray.fromUTF8(CrossChainDisputeIncoming));
+
+// arbitrator, _arbitrableChainId, _externalDisputeId, _templateId, _templateUri
+// note : arbitrable is an indexed arg, so it will topic[1]
+const CrossChainDisputeIncomingTypestring = "(address,uint256,uint256,uint256,string)";
+
+export const updateDisputeRequestData = (event: DisputeCreation): void => {
+  const dispute = Dispute.load(event.params._disputeID.toString());
+
+  if (!dispute) return;
+
+  const receipt = event.receipt;
+  if (!receipt) return;
+
+  const logs = receipt.logs;
+
+  // note that the topic at 0th index is always the event signature
+  const disputeRequestEventIndex = logs.findIndex((log) => log.topics[0] == DisputeRequestSignature);
+
+  const crossChainDisputeEventIndex = logs.findIndex((log) => log.topics[0] == CrossChainDisputeIncomingSignature);
+
+  if (crossChainDisputeEventIndex !== -1) {
+    const crossChainDisputeEvent = logs[crossChainDisputeEventIndex];
+
+    const decoded = ethereum.decode(CrossChainDisputeIncomingTypestring, crossChainDisputeEvent.data);
+
+    if (!decoded) return;
+
+    dispute.isCrossChain = true;
+    dispute.arbitrableChainId = decoded.toTuple()[1].toBigInt();
+    dispute.externalDisputeId = decoded.toTuple()[2].toBigInt();
+    dispute.templateId = decoded.toTuple()[3].toBigInt();
+    dispute.save();
+    return;
+  } else if (disputeRequestEventIndex !== -1) {
+    const disputeRequestEvent = logs[disputeRequestEventIndex];
+
+    const decoded = ethereum.decode(DisputeRequestTypestring, disputeRequestEvent.data);
+    if (!decoded) return;
+    dispute.isCrossChain = false;
+    dispute.arbitrableChainId = getChainId(dataSource.network());
+    dispute.externalDisputeId = decoded.toTuple()[0].toBigInt();
+    dispute.templateId = decoded.toTuple()[1].toBigInt();
+    dispute.save();
+    return;
+  }
+};
+
+// workaround, since hashmap don't work in subgraphs
+function getChainId(name: string): BigInt {
+  if (name == "arbitrum-one") return BigInt.fromI32(42161);
+  else if (name == "arbitrum-sepolia") return BigInt.fromI32(421614);
+  else return BigInt.fromI32(1);
 }
