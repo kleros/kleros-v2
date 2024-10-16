77b57e43d75441877467b20956f1a27ffac1214c
Harman-singh-waraich <Waraichharman68@gmail.com>
2024-10-16 15:58:25 +0530

    chore(kleros-sdk): define-entry-points-for-files

--- kleros-sdk/src/index.ts
+++ kleros-sdk/src/index.ts
@@ -1,4 +1,5 @@
 export * from "./sdk";
 export * from "./types";
-export * from "./utils/getDispute";
-export * from "./dataMappings/utils/disputeDetailsTypes";
+export * from "./utils";
+export * from "./dataMappings";
+export * from "./dataMappings/utils";
