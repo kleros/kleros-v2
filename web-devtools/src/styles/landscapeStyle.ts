a8e674610f4c5a58edd906ee65a01416df98cc74
Harman-singh-waraich <Waraichharman68@gmail.com>
2024-11-26 10:32:22 +0530

    fix: downgrade-styled-components

--- web-devtools/src/styles/landscapeStyle.ts
+++ web-devtools/src/styles/landscapeStyle.ts
@@ -1,8 +1,8 @@
-import { css, type RuleSet } from "styled-components";
+import { type DefaultTheme, type ThemeProps, type FlattenInterpolation, css } from "styled-components";
 
 export const BREAKPOINT_LANDSCAPE = 900;
 
-export const landscapeStyle = (styleFn: () => RuleSet<object>) => css`
+export const landscapeStyle = (styleFn: () => FlattenInterpolation<ThemeProps<DefaultTheme>>) => css`
   @media (min-width: ${BREAKPOINT_LANDSCAPE}px) {
     ${() => styleFn()}
   }
