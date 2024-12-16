b6aad1b82adac420cef1aa9ae36550a33519ce08
kemuru <102478601+kemuru@users.noreply.github.com>
2024-12-16 17:44:06 +0100

    chore: simplify styles

--- web/src/styles/commonStyles.ts
+++ web/src/styles/commonStyles.ts
@@ -1,21 +1,9 @@
 import { css } from "styled-components";
 
 export const hoverShortTransitionTiming = css`
-  :hover {
-    transition: 0.1s;
-  }
-
-  :not(:hover) {
-    transition: 0.1s;
-  }
+  transition: 0.1s;
 `;
 
 export const hoverLongTransitionTiming = css`
-  :hover {
-    transition: 0.2s;
-  }
-
-  :not(:hover) {
-    transition: 0.2s;
-  }
+  transition: 0.2s;
 `;
