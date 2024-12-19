d56e7c254f60cfaf2416e0d9327bfe096bc41822
jaybuidl <jb@kleros.io>
2024-12-19 16:00:58 +0000

    chore: ip check

--- web/scripts/generateBuildInfo.sh
+++ web/scripts/generateBuildInfo.sh
@@ -2,5 +2,8 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+echo "MY IP"
+curl ifconfig.me
+
 jq -n --arg primeUri "$DEPLOY_PRIME_URL" --arg uri "$URL" --arg deployUri "$DEPLOY_URL" '{ netlifyDeployPrimeUri: $primeUri, netlifyUri: $uri, netlifyDeployUri: $deployUri }' > src/generatedNetlifyInfo.json
 node $SCRIPT_DIR/gitInfo.js
