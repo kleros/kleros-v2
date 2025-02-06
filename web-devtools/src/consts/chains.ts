d36eaad991cb84b87edd393a374f63efe9ff77c5
Harman-singh-waraich <Waraichharman68@gmail.com>
2025-02-06 15:42:06 +0530

    chore(web-devtools): migrate-to-reown

--- web-devtools/src/consts/chains.ts
+++ web-devtools/src/consts/chains.ts
@@ -1,14 +1,14 @@
-import { type Chain, mainnet, arbitrumSepolia, gnosisChiado, arbitrum, gnosis } from "viem/chains";
+import { mainnet, arbitrumSepolia, gnosisChiado, arbitrum, gnosis, type AppKitNetwork } from "@reown/appkit/networks";
 
 import { isProductionDeployment } from "./index";
 
 export const DEFAULT_CHAIN = isProductionDeployment() ? arbitrum.id : arbitrumSepolia.id;
 
-export const SUPPORTED_CHAINS: Record<number, Chain> = {
+export const SUPPORTED_CHAINS: Record<number, AppKitNetwork> = {
   [isProductionDeployment() ? arbitrum.id : arbitrumSepolia.id]: isProductionDeployment() ? arbitrum : arbitrumSepolia,
 };
 
-export const QUERY_CHAINS: Record<number, Chain> = {
+export const QUERY_CHAINS: Record<number, AppKitNetwork> = {
   [isProductionDeployment() ? gnosis.id : gnosisChiado.id]: isProductionDeployment() ? gnosis : gnosisChiado,
   [mainnet.id]: mainnet,
 };
