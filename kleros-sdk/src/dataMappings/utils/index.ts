2ccc5cd5ba3f83b5fa54e06147458b78401acdf1
Harman-singh-waraich <Waraichharman68@gmail.com>
2025-02-11 19:02:16 +0530

    fix(kleros-sdk): fix-key-check

--- kleros-sdk/src/dataMappings/utils/index.ts
+++ kleros-sdk/src/dataMappings/utils/index.ts
@@ -1,3 +1,6 @@
 export * from "./populateTemplate";
 export * from "./retrieveVariables";
 export * from "./disputeDetailsTypes";
+
+export const isUndefined = (maybeObject: any): maybeObject is undefined | null =>
+  typeof maybeObject === "undefined" || maybeObject === null;
