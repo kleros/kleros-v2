fe34418786fb07bbe554922bfb4ba424e0ec37c3
kemuru <102478601+kemuru@users.noreply.github.com>
2025-02-21 18:18:46 +0100

    feat: add stakeset entity and handler

--- subgraph/core/src/SortitionModule.ts
+++ subgraph/core/src/SortitionModule.ts
@@ -4,8 +4,9 @@ import {
   StakeDelayedAlreadyTransferredWithdrawn,
   StakeDelayedNotTransferred,
   StakeLocked,
-  StakeSet,
+  StakeSet as StakeSetEvent,
 } from "../generated/SortitionModule/SortitionModule";
+import { Court, StakeSet as StakeSetEntity } from "../generated/schema";
 
 import { updateJurorDelayedStake, updateJurorStake } from "./entities/JurorTokensPerCourt";
 import { ensureUser } from "./entities/User";
@@ -23,7 +24,7 @@ export function handleStakeDelayedNotTransferred(event: StakeDelayedNotTransferr
   updateJurorDelayedStake(event.params._address.toHexString(), event.params._courtID.toString(), event.params._amount);
 }
 
-export function handleStakeSet(event: StakeSet): void {
+export function handleStakeSet(event: StakeSetEvent): void {
   const jurorAddress = event.params._address.toHexString();
   ensureUser(jurorAddress);
   const courtID = event.params._courtID.toString();
@@ -31,5 +32,18 @@ export function handleStakeSet(event: StakeSet): void {
   updateJurorStake(jurorAddress, courtID.toString(), SortitionModule.bind(event.address), event.block.timestamp);
   //stake is updated instantly so no delayed amount, set delay amount to zero
   updateJurorDelayedStake(jurorAddress, courtID, ZERO);
+
+  const generalCourt = Court.load("1");
+  if (!generalCourt) return;
+  const stakeSet = new StakeSetEntity(event.transaction.hash.toHex() + "-" + event.logIndex.toString());
+  stakeSet.address = event.params._address;
+  stakeSet.courtID = event.params._courtID;
+  stakeSet.stake = event.params._amount;
+  stakeSet.newTotalStake = generalCourt.effectiveStake;
+  stakeSet.blocknumber = event.block.number;
+  stakeSet.timestamp = event.block.timestamp;
+  stakeSet.logIndex = event.logIndex;
+  stakeSet.save();
 }
+
 export function handleStakeLocked(event: StakeLocked): void {}
