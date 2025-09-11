cbc111a9e55d89fa0b4f1297c6b6732f4841c07a
jaybuidl <jb@kleros.io>
2025-09-11 03:27:26 +0100

    chore: less optimizer iterations to shrink KlerosCore

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -32,7 +32,7 @@ const config: HardhatUserConfig = {
           viaIR: process.env.VIA_IR !== "false", // Defaults to true
           optimizer: {
             enabled: true,
-            runs: 2000,
+            runs: 1000,
           },
           outputSelection: {
             "*": {
