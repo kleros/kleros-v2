5b931abdf89f92ca0665d5576b243aa93b7a8a45
Harman-singh-waraich <Waraichharman68@gmail.com>
2026-01-30 11:46:57 +0530

    refactor(contracts): wagmi-artifact-rename

--- contracts/wagmi.config.hardhat.ts
+++ contracts/wagmi.config.hardhat.ts
@@ -3,9 +3,9 @@ import IHomeGateway from "./artifacts/src/gateway/interfaces/IHomeGateway.sol/IH
 import { getAbi, readArtifacts, merge } from "./scripts/wagmiHelpers";
 
 const getConfig = async (): Promise<Config> => {
-  const localhost = await readArtifacts("localhost");
-  localhost.forEach((c) => console.log("✔ Found localhost artifact: %s", c.name));
-  let contracts = localhost;
+  const artifact = await readArtifacts("localhost");
+  artifact.forEach((c) => console.log("✔ Found localhost artifact: %s", c.name));
+  let contracts = artifact;
 
   const chiadoContracts = await readArtifacts("gnosisChiado", "chiadoDevnet"); // renaming the Hardhat network improves this but breaks many other scripts
   chiadoContracts.forEach((c) => console.log("✔ Found chiado artifact: %s", c.name));
