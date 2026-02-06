2382d0a31e2ed6f1277dba27bf065991cf8e24a1
Harman-singh-waraich <Waraichharman68@gmail.com>
2026-02-06 18:13:06 +0530

    refactor(web): hash-vote-documentation-update

--- web/src/utils/crypto/hashVote.ts
+++ web/src/utils/crypto/hashVote.ts
@@ -1,11 +1,13 @@
-import { encodePacked, keccak256 } from "viem";
+import { encodePacked, type Hash, keccak256 } from "viem";
+
+export type Bytes32Hash = Hash;
 
 /**
  * Computes the hash of a vote
  *
  * @param choice The choice being voted for
  * @param salt A random salt for commitment
- * @return bytes32 The hash of the encoded vote parameters
+ * @returns Bytes32Hash A keccak256 hash (bytes32) derived from the encoded vote parameters
  */
 export const hashVote = (choice: bigint, salt: bigint): `0x${string}` => {
   const encoded = encodePacked(["uint256", "uint256"], [choice, salt]);
