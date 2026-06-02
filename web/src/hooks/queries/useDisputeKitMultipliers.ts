f53dd48179aa1fd9a5197420db3a6e635d2325dd
Harman-singh-waraich <Waraichharman68@gmail.com>
2026-06-02 15:16:52 +0530

    refactor(web): remove-classic-reference-in-use-kit-multiplier-hook

--- web/src/hooks/queries/useDisputeKitMultipliers.ts
+++ web/src/hooks/queries/useDisputeKitMultipliers.ts
@@ -11,7 +11,7 @@ export const useDisputeKitMultipliers = (disputeKitAddress?: Address) => {
   const isEnabled = !isUndefined(publicClient) && !isUndefined(disputeKitAddress);
 
   return useQuery({
-    queryKey: [`DisputeKitClassicMultipliers`, disputeKitAddress],
+    queryKey: [`DisputeKitMultipliers`, disputeKitAddress],
     enabled: isEnabled,
     staleTime: Infinity,
     queryFn: async () => {
@@ -19,16 +19,16 @@ export const useDisputeKitMultipliers = (disputeKitAddress?: Address) => {
       const config = getDisputeKitConfigByAddress(disputeKitAddress);
       if (isUndefined(config)) return;
 
-      const disputeKitClassic = getContract({
+      const disputeKit = getContract({
         abi: config.disputeKitAbi,
         address: config.address,
         client: {
           public: publicClient,
         },
       });
-      const winner_stake_multiplier = await disputeKitClassic.read.WINNER_STAKE_MULTIPLIER();
-      const loser_stake_multiplier = await disputeKitClassic.read.LOSER_STAKE_MULTIPLIER();
-      const loser_appeal_period_multiplier = await disputeKitClassic.read.LOSER_APPEAL_PERIOD_MULTIPLIER();
+      const winner_stake_multiplier = await disputeKit.read.WINNER_STAKE_MULTIPLIER();
+      const loser_stake_multiplier = await disputeKit.read.LOSER_STAKE_MULTIPLIER();
+      const loser_appeal_period_multiplier = await disputeKit.read.LOSER_APPEAL_PERIOD_MULTIPLIER();
       return {
         winner_stake_multiplier,
         loser_stake_multiplier,
@@ -37,9 +37,3 @@ export const useDisputeKitMultipliers = (disputeKitAddress?: Address) => {
     },
   });
 };
-
-export interface IDisputeKitClassicMultipliers {
-  winner_stake_multiplier: bigint;
-  loser_stake_multiplier: bigint;
-  loser_appeal_period_multiplier: bigint;
-}
