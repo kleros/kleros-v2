7828d66e443f8d4d034300d6e109125c94b1641f
Harman-singh-waraich <Waraichharman68@gmail.com>
2026-06-04 16:11:24 +0530

    chore(web): update-atlas-provider

--- web/src/context/AtlasProvider.tsx
+++ web/src/context/AtlasProvider.tsx
@@ -2,12 +2,19 @@ import React from "react";
 
 import { useConfig } from "wagmi";
 
-import { AtlasProvider as _AtlasProvider, Products } from "@kleros/kleros-app";
+import { AtlasProvider as _AtlasProvider, IpfsProduct, SignupProduct } from "@kleros/kleros-app";
 
 const AtlasProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const wagmiConfig = useConfig();
   return (
-    <_AtlasProvider config={{ uri: import.meta.env.REACT_APP_ATLAS_URI, product: Products.CourtV2, wagmiConfig }}>
+    <_AtlasProvider
+      config={{
+        uri: import.meta.env.REACT_APP_ATLAS_URI,
+        signupProduct: SignupProduct.CourtV2,
+        wagmiConfig,
+        ipfsProduct: IpfsProduct.CourtV2,
+      }}
+    >
       {children}
     </_AtlasProvider>
   );
